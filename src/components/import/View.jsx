e7975a93efcff43f13d1c2339a350adf953dab24
Cory Jones <cory.jones@nextthought.com>
2017-09-15 09:54:47 -0500

    Import widget UI in place

--- src/components/import/View.jsx
+++ src/components/import/View.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {scoped} from 'nti-lib-locale';
+import {Input} from 'nti-web-commons';
 
 const LABELS = {
 	defaultTitle: 'Import Course',
 	cancel: 'Cancel',
-	import: 'Import'
+	import: 'Import',
+	identifier: 'Identifier',
+	adminLevel: 'Admin Level',
+	importFile: 'Import File'
 };
 
 const t = scoped('COURSE_IMPORT', LABELS);
@@ -19,6 +23,8 @@ export default class CourseImport extends React.Component {
 
 	constructor (props) {
 		super(props);
+
+		this.state = {};
 	}
 
 	cancel = () => {
@@ -46,8 +52,36 @@ export default class CourseImport extends React.Component {
 		</div>);
 	}
 
+	updateAdminLevel = (value) => {
+		this.setState({adminLevel : value});
+	}
+
+	updateIDNumber = (value) => {
+		this.setState({identifier : value});
+	}
+
+	updateImportFile = (file) => {
+		this.setState({file});
+	}
+
+	renderAdminLevel () {
+		return (<Input.Text placeholder={t('adminLevel')} value={this.state.adminLevel} onChange={this.updateAdminLevel}/>);
+	}
+
+	renderIDInput () {
+		return (<Input.Text placeholder={t('identifier')} value={this.state.identifier} onChange={this.updateIDNumber}/>);
+	}
+
+	renderFileImport () {
+		return (<div className="import-file"><Input.File placeholder={t('importFile')} onFileChange={this.updateImportFile}/></div>);
+	}
+
 	renderBody () {
-		return (<div className="course-import-content"/>);
+		return (<div className="course-panel-getstarted-form">
+			{this.renderAdminLevel()}
+			{this.renderIDInput()}
+			{this.renderFileImport()}
+		</div>);
 	}
 
 	renderFooter () {
