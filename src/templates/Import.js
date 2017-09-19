f2c76bc188d7903096cf177df5c353c964a0a07f
chris.utz <chris.utz@nextthought.com>
2017-09-19 18:06:07 -0500

    fix typo

--- src/templates/Import.js
+++ src/templates/Import.js
@@ -2,7 +2,7 @@ import { ImportPanel } from '../components/panels';
 
 export const Import = {
 	name: 'Import',
-	description: 'Create a new course by importing and existing course',
+	description: 'Create a new course by importing an existing course',
 
 	panels: [
 		ImportPanel
