50509b1bdc786450f01538b7670f24df1220d605
Cory Jones <cory.jones@nextthought.com>
2017-11-14 15:29:51 -0600

    Placeholder for canEdit logic for facilitators

---
+++ src/editor/inline/components/facilitators/__test__/Edit.spec.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Edit from '../Edit';
+
+const mockService = () => ({
+	getObject: (o) => Promise.resolve(o)
+});
+
+const onBefore = () => {
+	global.$AppConfig = {
+		...(global.$AppConfig || {}),
+		nodeService: mockService(),
+		nodeInterface: {
+			getServiceDocument: () => Promise.resolve(global.$AppConfig.nodeService)
+		}
+	};
+};
+
+const onAfter = () => {
+	//unmock getService()
+	const {$AppConfig} = global;
+	delete $AppConfig.nodeInterface;
+	delete $AppConfig.nodeService;
+};
+
+const facilitators = [
+	{
+		role: 'assistant',
+		visible: true,
+		JobTitle: 'visible',
+		Name: 'Visible Assistant',
+		username: 'visibleAssistant',
+		MimeType: 'application/vnd.nextthought.courses.coursecataloginstructorlegacyinfo',
+		Class: 'CourseCatalogInstructorLegacyInfo'
+	},
+	{
+		role: 'editor',
+		visible: false,
+		JobTitle: 'hidden',
+		Name: 'Hidden Editor',
+		username: 'hiddenEditor',
+		MimeType: 'application/vnd.nextthought.courses.coursecataloginstructorlegacyinfo',
+		Class: 'CourseCatalogInstructorLegacyInfo'
+	}
+];
+
+/* eslint-env jest */
+describe('Facilitators edit test', () => {
+	beforeEach(onBefore);
+	afterEach(onAfter);
+
+	test('Test editable', () => {
+		const courseInstance = {
+			hasLink: (l) => l === 'Instructors' || l === 'Editors'
+		};
+
+		const cmp = mount(<Edit facilitators={facilitators} courseInstance={courseInstance}/>);
+
+		const facilitatorItems = cmp.find('.facilitator.edit');
+
+		expect(facilitatorItems.length).toBe(2);
+	});
+
+	test('Test non-editable (assistant role)', () => {
+		const courseInstance = {
+			hasLink: (l) => l === 'Instructors'
+		};
+
+		const cmp = mount(<Edit facilitators={facilitators} courseInstance={courseInstance}/>);
+
+		const editableItems = cmp.find('.facilitator.edit');
+
+		// only Insturctor role should be able to edit
+		expect(editableItems.exists()).toBe(false);
+
+		const facilitatorItems = cmp.find('.facilitator');
+
+		expect(facilitatorItems.length).toBe(2);
+	});
+
+	test('Test non-editable (editor role)', () => {
+		const courseInstance = {
+			hasLink: (l) => l === 'Editors'
+		};
+
+		const cmp = mount(<Edit facilitators={facilitators} courseInstance={courseInstance}/>);
+
+		const editableItems = cmp.find('.facilitator.edit');
+
+		// only Insturctor role should be able to edit
+		expect(editableItems.exists()).toBe(false);
+
+		const facilitatorItems = cmp.find('.facilitator');
+
+		expect(facilitatorItems.length).toBe(2);
+	});
+});
