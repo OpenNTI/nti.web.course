7a96f01dd775105ec56d80985ec1a6817c711001
Cory Jones <cory.jones@nextthought.com>
2017-11-14 11:15:41 -0600

    Export CourseCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export CourseListing from './listing/CourseListing';
 export CourseCreateButton from './CreateButton';
 export CourseMenu from './listing/CourseMenu';
+export CourseCard from './listing/CourseCard';
