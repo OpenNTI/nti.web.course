51681217356b6588931819e9f4d612abe0243eda
Josh Birdwell <jbirdwell34@gmail.com>
2018-01-24 16:10:10 -0600

    fix git ignore add reports

---
+++ src/admin-tools/reports/View.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Widgets, List} from 'nti-web-reports';
+
+const { Card } = Widgets;
+
+CourseAdminCourseReports.propTypes = {
+	course: PropTypes.object
+};
+
+export default function CourseAdminCourseReports ({course}) {
+	return course ?
+		(<Card><List context={course} /></Card>) :
+		null;
+}
