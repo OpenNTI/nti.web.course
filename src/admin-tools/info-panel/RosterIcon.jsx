d22888f888b3605ae7bdcbfbe2dbb78972c33024
Josh Birdwell <jbirdwell34@gmail.com>
2018-01-25 22:20:10 -0600

    update default

--- src/admin-tools/info-panel/RosterIcon.jsx
+++ src/admin-tools/info-panel/RosterIcon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RosterIcon = ({ totalLearners = '' }) => (
+const RosterIcon = ({ totalLearners = 0 }) => (
 	<div className="admin-roster-icon">
 		<div className="roster-total-learners">{totalLearners}</div>
 		<div className="roster-header">LEARNERS</div>
