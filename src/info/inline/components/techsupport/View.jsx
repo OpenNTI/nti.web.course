bc462352230c829cf94ff8aebec5e2dae724f617
Josh Birdwell <jbirdwell34@gmail.com>
2018-02-06 13:30:30 -0600

    NTI-4990: Correct links for course-info
    Make sure there are default for the webapp

--- src/info/inline/components/techsupport/View.jsx
+++ src/info/inline/components/techsupport/View.jsx
@@ -3,28 +3,46 @@ import { scoped } from 'nti-lib-locale';
 
 import TechsupportLink from './TechsupportLink';
 
-const TECHSUPPORT_LABELS = {
-	support: 'Support',
-	info: 'Info',
-	website: 'NextThought Website',
-	help: 'Help Site'
+const DEFAULT_TEXT = {
+	label: 'Tech Support',
+	LINK0: {
+		label: 'Support',
+		link: 'mailto:support@nextthought.com'
+	},
+	LINK1: {
+		label: 'Info',
+		link: 'mailto:info@nextthought.com'
+	},
+	LINK2: {
+		label: 'NextThought Website',
+		link: 'http://nextthought.com'
+	},
+	LINK3: {
+		label: 'Help Site',
+		link: 'https://help.nextthought.com/'
+	}
 };
+const t = scoped('COURSE.CONTACTINFO', DEFAULT_TEXT);
 
-const t = scoped('components.course.editor.inline.components.techsupport.View', TECHSUPPORT_LABELS);
+const renderLink = index => {
+	const label = t(`LINK${index}.label`);
+	const link = t(`LINK${index}.link`);
+	return (
+		<TechsupportLink href={link} label={label} />
+	);
+};
 
 const TechSupport = () => (
 	<div className="course-info-support">
-		<h3 className="techsupport-header">Tech Support</h3>
+		<h3 className="techsupport-header">{t('label')}</h3>
 		<div className="techsupport-info">
 			<div className="techsupport-image" />
 			<ul className="techsupport-links">
-				<TechsupportLink href="mailto:support@nextthought.com" label={t('support')} />
-				<TechsupportLink href="mailto:info@nextthought.com" label={t('info')} />
-				<TechsupportLink href="http://nextthought.com" label={t('website')} />
-				<TechsupportLink href="http://help.nextthought.com/" label={t('help')} />
+				{[0,1,2,3].map(x => renderLink(x))}
 			</ul>
 		</div>
 	</div>
 );
+
 
 export default TechSupport;
