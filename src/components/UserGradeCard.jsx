0b6de51a8248d47f66d7a23a009f148619dbd4bb
Cory Jones <cory.jones@nextthought.com>
2018-02-20 09:51:46 -0600

    Initial user grade card component

---
+++ src/components/UserGradeCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Avatar, DisplayName} from 'nti-web-commons';
+import { scoped } from 'nti-lib-locale';
+
+const t = scoped('course.components.UserGradeCard', {
+	assignGrade: 'Assign Final Grade',
+	notAvailable: '-'
+});
+
+export default class UserGradeCard extends React.Component {
+	static propTypes = {
+		user: PropTypes.oneOf([PropTypes.object, PropTypes.string]),
+		grade: PropTypes.number,
+		onAssignGrade: PropTypes.func
+	}
+
+	onAssignClick = () => {
+		const { onAssignGrade } = this.props;
+
+		onAssignGrade && onAssignGrade();
+	}
+
+	renderAssignGrade () {
+		return <div className="assign-grade" onClick={this.onAssignClick}>{t('assignGrade')}</div>;
+	}
+
+	render () {
+		const { user, grade } = this.props;
+
+		return (
+			<div className="user-grade-card">
+				<div className="avatar-container">
+					<Avatar className="avatar" entity={user}/>
+					<div className="grade-container">
+						<div className="grade">{grade || t('notAvailable')}</div>
+					</div>
+				</div>
+				<div className="user-name"><DisplayName entity={user}/></div>
+				{this.renderAssignGrade()}
+			</div>
+		);
+	}
+}
