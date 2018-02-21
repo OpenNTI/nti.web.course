0691be62a219d3f8e86b1e49240b9876b5ea731f
Cory Jones <cory.jones@nextthought.com>
2018-02-21 08:49:32 -0600

    Fix test

--- src/info/inline/components/identifier/__test__/Edit.spec.js
+++ src/info/inline/components/identifier/__test__/Edit.spec.js
@@ -5,16 +5,23 @@ import Edit from '../Edit';
 
 /* eslint-env jest */
 describe('Department edit test', () => {
-	// Currently, there is no identifier editor
-	test('Test that it does nothing!', () => {
+	const onChange = jest.fn();
+
+	test('Test identifier edit', () => {
 		const identifier = 'ABC123';
 
 		const catalogEntry = {
 			'ProviderUniqueID': identifier
 		};
 
-		const cmp = mount(<Edit catalogEntry={catalogEntry}/>);
+		const cmp = mount(<Edit onValueChange={onChange} catalogEntry={catalogEntry}/>);
 
-		expect(cmp.html()).toEqual(null);
+		const input = cmp.find('.identifier-input').first();
+
+		expect(input.props().value).toEqual(identifier);
+
+		input.simulate('change');
+
+		expect(onChange).toHaveBeenCalledWith('ProviderUniqueID', identifier);
 	});
 });
