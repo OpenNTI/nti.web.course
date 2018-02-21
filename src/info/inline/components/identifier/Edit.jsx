4b54005dc1e85ed6b1d5ba12ccf54bdfd2327197
Cory Jones <cory.jones@nextthought.com>
2018-02-21 08:41:17 -0600

    NTI-5137 Add ID editing to course info panel

--- src/info/inline/components/identifier/Edit.jsx
+++ src/info/inline/components/identifier/Edit.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {scoped} from 'nti-lib-locale';
+import {Input} from 'nti-web-commons';
+
+
+const t = scoped('course.info.inline.components.identifier.Edit', {
+	label: 'Course Identifier',
+	titleIsRequired: 'Identifier is required'
+});
 
 export default class IdentifierEdit extends React.Component {
 	static propTypes = {
-		catalogEntry: PropTypes.object.isRequired
+		catalogEntry: PropTypes.object.isRequired,
+		onValueChange: PropTypes.func,
+		error: PropTypes.string
 	}
 
-	// no static FIELD_NAME because identifier is not editable currently
+	static FIELD_NAME = 'ProviderUniqueID';
 
 	constructor (props) {
 		super(props);
 
-		this.state = {};
+		this.state = { value: props.catalogEntry[IdentifierEdit.FIELD_NAME] };
+	}
+
+	onChange = (val) => {
+		const { onValueChange } = this.props;
+
+		this.setState({value: val});
+
+		onValueChange && onValueChange(IdentifierEdit.FIELD_NAME, val);
+	}
+
+	renderError () {
+		const { error } = this.props;
+
+		if(error) {
+			return <div className="error">{t('titleIsRequired')}</div>;
+		}
 	}
 
 	render () {
-		return null; // no editor for identifier
+		return (
+			<div className="identifier-editor">
+				{this.renderError()}
+				<div className="label">{t('label')}</div>
+				<Input.Text className="identifier-input" onChange={this.onChange} value={this.state.value} maxLength="140"/>
+			</div>
+		);
 	}
 }
