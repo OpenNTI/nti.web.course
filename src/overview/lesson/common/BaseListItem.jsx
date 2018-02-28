90f34732dfd02d9b81f49d1623733b2573f9fdc3
Andrew Ligon <andrew.ligon@nextthought.com>
2018-02-28 14:46:40 -0600

    Add a video list item

---
+++ src/overview/lesson/common/BaseListItem.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {List} from 'nti-web-commons';
+
+import PaddedContainer from './PaddedContainer';
+
+export default class LessonOverviewBaseListItem extends React.Component {
+	static propTypes = {
+		className: PropTypes.string,
+		item: PropTypes.object,
+
+		title: PropTypes.string,
+		labels: PropTypes.array,
+
+		renderTitle: PropTypes.func,
+		renderLabels: PropTypes.func,
+		renderIcon: PropTypes.func
+	}
+
+
+	render () {
+		const {className} = this.props;
+
+		return (
+			<PaddedContainer className={cx('lesson-overview-base-list-item', className)}>
+				<div className="icon-container">
+					{this.renderIcon()}
+				</div>
+				<div className="right">
+					<div className="title">
+						{this.renderTitle()}
+					</div>
+					<div className="labels">
+						{this.renderLabels()}
+					</div>
+				</div>
+			</PaddedContainer>
+		);
+	}
+
+	renderIcon () {
+		const {renderIcon} = this.props;
+
+		return renderIcon ? renderIcon() : null;
+	}
+
+
+	renderTitle () {
+		const {title, item, renderTitle} = this.props;
+
+		if (renderTitle) { return renderTitle(); }
+
+		return title != null ? title : (item.title || item.label);
+	}
+
+
+	renderLabels () {
+		const {labels, renderLabels} = this.props;
+
+		if (renderLabels) { return renderLabels(); }
+
+		if (!labels) { return null; }
+
+		return (
+			<List.SeparatedInline>
+				{labels}
+			</List.SeparatedInline>
+		);
+	}
+}
--- src/overview/lesson/items/video/List.jsx
+++ src/overview/lesson/items/video/List.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {DateTime} from 'nti-web-commons';
 
-export default function LessonOverviewVideoList () {
-	return (
-		<div>
-			Video List Item
-		</div>
-	);
+import Base from '../../common/BaseListItem';
+
+export default class LessonOverviewVideoListItem extends React.Component {
+	static propTypes = {
+		item: PropTypes.object
+	}
+
+	state = {poster: null}
+
+	componentWillReceiveProps (nextProps) {
+		const {item:nextItem} = nextProps;
+		const {item:oldItem} = this.props;
+
+		if (nextItem !== oldItem) {
+			this.setupFor(nextProps);
+		}
+	}
+
+	componentDidMount () {
+		this.setupFor(this.props);
+	}
+
+
+	async setupFor (props) {
+		const {item} = this.props;
+
+		try {
+			const thumb = await item.getThumbnail();
+			const duration = await item.getDuration();
+
+			this.setState({thumb, duration});
+		} catch (e) {
+			//Its alright if it fails. Nothing to do here
+		}
+	}
+
+	render () {
+		const {item} = this.props;
+		const {duration} = this.state;
+		const formattedDuration = duration != null ? DateTime.formatDuration(duration) : '';
+
+		return (
+			<Base
+				className="lesson-overview-video-list-item"
+				item={item}
+				renderIcon={this.renderIcon}
+				labels={[formattedDuration]}
+			/>
+		);
+	}
+
+
+	renderIcon = () => {
+		const {thumb} = this.state;
+
+		return (
+			<div className="lesson-overview-video-list-item-icon">
+				{thumb && (<img src={thumb} />)}
+			</div>
+		);
+	}
 }
