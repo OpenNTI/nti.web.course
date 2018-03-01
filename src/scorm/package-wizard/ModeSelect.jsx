4348d6ef2a9d2c8909f9312200a4ad7b19260a03
Cory Jones <cory.jones@nextthought.com>
2018-03-01 16:59:11 -0600

    First pass at package update/replace wizard

--- src/scorm/package-wizard/ModeSelect.jsx
+++ src/scorm/package-wizard/ModeSelect.jsx
@@ -1,19 +1,118 @@
 import React from 'react';
-import Template from './template';
+import PropTypes from 'prop-types';
+import {scoped} from 'nti-lib-locale';
+import cx from 'classnames';
 
-const ModeSelect = ({ onModeSelect }) => {
-	return (
-		<div className="course-panel-templatechooser">
-			<div className="course-panel-content">
-				<div className="options-container">
-					<Template />
-					<Template />
+// TODO: Update the labels to match design
+// Also, need to style based on design
+const t = scoped('course.scorm.package-wizard.ModeSelect', {
+	cancel: 'Cancel',
+	updateTitle: 'Update',
+	updateDescription: 'Update package',
+	updateHint: 'Better for small updates',
+	replaceTitle: 'Replace',
+	replaceDescription: 'Replace package',
+	replaceHint: 'Better for big updates'
+});
+
+// TODO: Match the update key to match the server API.  Replace key should already
+// be fine since ImportSCORM is the link for uploading a new Scorm package
+export const UPDATE_KEY = 'UpdateSCORM';
+export const REPLACE_KEY = 'ImportSCORM';
+
+export default class ModeSelect extends React.Component {
+	static propTypes = {
+		onModeSelect: PropTypes.func.isRequired,
+		saveCmp: PropTypes.func,
+		onCancel: PropTypes.func,
+		buttonLabel: PropTypes.string
+	}
+
+
+	state = {}
+
+
+	renderModeOption (name, description, hint, selected, clickHandler) {
+		const className = cx('mode-option', { selected });
+
+		return (
+			<div className={className} onClick={clickHandler}>
+				<div className="name">{name}</div>
+				<div className="description">{description}</div>
+				<div className="hint">{hint}</div>
+			</div>
+		);
+	}
+
+
+	onSave = (done) => {
+		const { onModeSelect } = this.props;
+
+		onModeSelect && onModeSelect(this.state.selected);
+
+		done();
+	}
+
+
+	renderSaveCmp () {
+		const { buttonLabel, saveCmp: Cmp } = this.props;
+
+		if(Cmp) {
+			return (<Cmp onSave={this.onSave} label={buttonLabel}/>);
+		}
+
+		return null;
+	}
+
+
+	renderCancelCmp () {
+		if(this.props.onCancel) {
+			return (<div className="course-panel-cancel" onClick={this.props.onCancel}>{t('cancel')}</div>);
+		}
+	}
+
+
+	updateSelected = (template) => {
+		this.setState({selected: template});
+	}
+
+
+	selectUpdate = () => {
+		this.setState({selected: UPDATE_KEY});
+	}
+
+
+	selectReplace = () => {
+		this.setState({selected: REPLACE_KEY});
+	}
+
+
+	render () {
+		const {selected} = this.state;
+
+		return (
+			<div className="course-panel-templatechooser">
+				<div className="course-panel-content">
+					<div className="mode-options-container">
+						{this.renderModeOption(
+							t('updateTitle'),
+							t('updateDescription'),
+							t('updateHint'),
+							UPDATE_KEY === selected,
+							this.selectUpdate)}
+						{this.renderModeOption(
+							t('replaceTitle'),
+							t('replaceDescription'),
+							t('replaceHint'),
+							REPLACE_KEY === selected,
+							this.selectReplace)}
+					</div>
+				</div>
+				<div className="course-panel-controls">
+					{this.renderSaveCmp()}
+					{this.renderCancelCmp()}
 				</div>
 			</div>
-			<div className="course-panel-controls">
-				{this.renderSaveCmp()}
-				{this.renderCancelCmp()}
-			</div>
-		</div>
-	)
-});
+		);
+	}
+}
--- src/scorm/package-wizard/PackageWizard.jsx
+++ src/scorm/package-wizard/PackageWizard.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getService } from 'nti-web-client';
-import { Models } from 'nti-lib-interfaces';
 import { Switch } from 'nti-web-commons';
 import { scoped } from 'nti-lib-locale';
 
-import { Blank } from '../templates/Blank';
-import { Import } from '../templates/Import';
+import WizardItem from '../../editor/wizard/WizardItem';
+import ScormImport from '../../editor/panels/scorm-import/WizardPanel';
+import ImportConfirmation from '../../editor/panels/importconfirmation/WizardPanel';
 
-import TemplateChooser from './TemplateChooser';
-import WizardItem from './WizardItem';
-import Scorm from '../../scorm/View';
+import ModeSelect, {UPDATE_KEY} from './ModeSelect';
 
-
+// TODO: Update labels to match design
 const t = scoped('course.wizard.CourseWizard', {
 	finish: 'Finish',
-	chooseTemplate: 'Choose creation type'
+	chooseMode: 'Change SCORM Package',
+	updateTitle: 'Update SCORM Package',
+	replaceTitle: 'Replace SCORM Package'
 });
 
 export default class CourseWizard extends React.Component {
@@ -27,66 +26,21 @@ export default class CourseWizard extends React.Component {
 		onCourseModified: PropTypes.func
 	}
 
-	state = {
-		availableTemplates: [Update, Replace]
-	};
 
-	async componentDidMount() {
-		const service = await getService();
-		const courseWorkspace = service.getWorkspace('Courses');
-		const allCoursesCollection = courseWorkspace && service.getCollection('AllCourses', courseWorkspace.Title);
-		if (allCoursesCollection.accepts[Models.courses.scorm]) {
-			this.setState({ availableTemplates: [...this.state.availableTemplates, Scorm] });
-		}
-	}
+	state = {}
+
 
 	cancel = () => {
 		this.props.onCancel && this.props.onCancel();
 	}
 
-	creationCompleted = (allowRedirect) => {
-		const { onFinish } = this.props;
-
-		onFinish && onFinish(this.state.catalogEntry);
-	}
-
-	renderItem = (panel, index, arr) => {
-		const stepName = 'step' + (index + 2);
-
-		return (<Switch.Item
-			className="course-panel-content"
-			key={stepName}
-			name={stepName}
-			component={WizardItem}
-			wizardCmp={panel.WizardPanel}
-			catalogEntry={this.state.catalogEntry}
-			stepName={panel.tabDescription}
-			onCancel={this.cancel}
-			hideHeaderControls={panel.WizardPanel.hideHeaderControls}
-			afterSave={index === arr.length - 1
-				? () => this.creationCompleted(!panel.WizardPanel.disallowEditorRedirect)
-				: () => { }} />);
-	}
-
-
-	renderItems() {
-		if (this.state.panels) {
-			return (this.state.panels).map(this.renderItem);
-		}
-
-		// return a placeholder if we haven't chosen a template yet
-		return (<Switch.Item
-			className="course-panel-content"
-			name="step2"
-			component={WizardItem}
-			wizardCmp={TemplateChooser} />);
-	}
 
 	onModeSelect = (mode) => {
 		this.setState({ mode });
 	}
 
-	renderMode() {
+
+	renderMode () {
 		return (
 			<Switch.Item
 				className="course-panel-content"
@@ -96,16 +50,60 @@ export default class CourseWizard extends React.Component {
 				stepName={t('chooseMode')}
 				onModeSelect={this.onModeSelect}
 				onCancel={this.cancel}
-				firstTab 
+				firstTab
 			/>
 		);
 	}
 
-	render() {
+
+	afterSave () {
+		// TODO: What should happen after save?
+	}
+
+
+	renderUpload () {
+		// TODO: provide the appropriate link to the import panel based on the current mode
+		// TODO: fix the step name and title based on design
+		return (
+			<Switch.Item
+				className="course-panel-content"
+				key="scormUpload"
+				name="scormUpload"
+				component={WizardItem}
+				wizardCmp={ScormImport}
+				catalogEntry={this.state.catalogEntry}
+				title="Change SCORM Package"
+				stepName={UPDATE_KEY === this.state.mode ? t('updateTitle') : t('replaceTitle')}
+				onCancel={this.cancel}
+				afterSave={this.afterSave} />
+		);
+	}
+
+
+	renderConfirmation () {
+		// TODO: fix the step name and title based on design
+		return (
+			<Switch.Item
+				className="course-panel-content"
+				key="scormUploadConfirmation"
+				name="scormUploadConfirmation"
+				component={WizardItem}
+				wizardCmp={ImportConfirmation}
+				catalogEntry={this.state.catalogEntry}
+				title="Change SCORM Package"
+				stepName="Confirmation"
+				onCancel={this.cancel}
+				afterSave={this.afterSave} />
+		);
+	}
+
+
+	render () {
 		return (<Switch.Panel className="course-panel" active="TemplateChooser">
 			<Switch.Container>
 				{this.renderMode()}
-				{this.renderItems()}
+				{this.renderUpload()}
+				{this.renderConfirmation()}
 			</Switch.Container>
 		</Switch.Panel>);
 	}
