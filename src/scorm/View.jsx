e80e01ee9fe55b75de969f7aabd330b6ee759680
Cory Jones <cory.jones@nextthought.com>
2018-03-05 10:38:27 -0600

    Embed scorm iframe in page, add redirect handling to reset state

--- src/scorm/View.jsx
+++ src/scorm/View.jsx
@@ -1,10 +1,15 @@
+import URL from 'url';
+import {join} from 'path';
+
 import React, { Component } from 'react';
 import { Button } from 'nti-web-commons';
 import PropTypes from 'prop-types';
+import {resolveBasePath} from 'nti-web-client';
 
 import Editor from './Editor';
 
 const IMPORT_SCORM = 'ImportScorm';
+const STATIC_REDIRECT = 'scormredirect.html';
 
 class Scorm extends Component {
 	static propTypes = {
@@ -19,6 +24,49 @@ class Scorm extends Component {
 		scormLink: '',
 		showEditor: false,
 		courseLaunched: false
+	}
+
+	constructor (props) {
+		super(props);
+
+		const url = URL.parse(global.location.href);
+
+		url.search = null;
+		url.hash = null;
+		url.query = null;
+		url.pathname = join(resolveBasePath(), STATIC_REDIRECT);
+
+		this.redirecturl = url.format();
+	}
+
+	componentDidMount () {
+		window.addEventListener('message', this.onClose, false);
+	}
+
+	attachRef = (x) => {
+		this.iframe = x;
+	}
+
+	onClose = (message) => {
+		if(message) {
+			let data;
+
+			try {
+				data = JSON.parse(message.data) || {};
+			}
+			catch (e) {
+				data = {};
+			}
+
+			if('scorm-exit' === data.message) {
+				this.setState({courseLaunched: false, scormLink: ''});
+				return;
+			}
+		}
+	}
+
+	componentWillUnmount () {
+		window.removeEventListener('message', this.onClose);
 	}
 
 	componentDidUpdate (prevProps) {
@@ -44,13 +92,23 @@ class Scorm extends Component {
 		this.setState({ showEditor: false });
 	}
 
+	renderFrame () {
+		return (
+			<iframe
+				ref={this.attachRef}
+				seamless="true"
+				frameBorder="0"
+				src={this.state.scormLink + '?redirecturl=' + encodeURIComponent(this.redirecturl)} />
+		);
+	}
+
 	renderInstructor = () => {
-		const { showEditor, courseLaunched } = this.state;
+		const { showEditor, courseLaunched, scormLink } = this.state;
 		const { bundle } = this.props;
 
 		return (
 			<div className="scorm-card scorm-instructor-card">
-				<div className="scorm-title">{bundle.title}</div >
+				<div className="scorm-title">{bundle.title}</div>
 				<a className="scorm-edit-link" onClick={this.editScorm}>Change Content Package</a>
 				<div className="scorm-desc">
 					This course uses an external website for displaying content.
@@ -58,7 +116,8 @@ class Scorm extends Component {
 					{courseLaunched && 'If you do not see it after, a popup blocker may be preventing it from opening. Please disable popup blockers for this site.'}
 				</div>
 				{!courseLaunched && <Button className="scorm-launch-button" onClick={this.launchCourse}>Open</Button>}
-				{courseLaunched && <Button className="scorm-post-launch-button" disabled>Already Open</Button>}
+				{scormLink !== '' && this.renderFrame()}
+				{courseLaunched && !scormLink && <Button className="scorm-post-launch-button" disabled>Already Open</Button>}
 				{showEditor && <Editor onDismiss={this.onDismiss} bundle={this.props.bundle} />}
 			</div>
 		);
@@ -66,7 +125,7 @@ class Scorm extends Component {
 
 	renderStudent = () => {
 		const { bundle } = this.props;
-		const { courseLaunched } = this.state;
+		const { courseLaunched, scormLink } = this.state;
 
 		return (
 			<div className="scorm-card scorm-student-card">
@@ -74,11 +133,12 @@ class Scorm extends Component {
 					<div className="scorm-title">{ bundle.title }</div>
 					<div className="scorm-desc">
 						This course uses an external website for displaying content.
-						Follow the link below to access your course content. 
+						Follow the link below to access your course content.
 						{courseLaunched && 'If you do not see it after, a popup blocker may be preventing it from opening. Please disable popup blockers for this site.'}
 					</div>
 					{!courseLaunched && <Button className="scorm-launch-button" onClick={this.launchCourse}>Open</Button>}
-					{courseLaunched && <Button className="scorm-post-launch-button" disabled>Already Open</Button>}
+					{scormLink !== '' && this.renderFrame()}
+					{courseLaunched && !scormLink && <Button className="scorm-post-launch-button" disabled>Already Open</Button>}
 				</div>
 			</div>
 		);
@@ -91,7 +151,6 @@ class Scorm extends Component {
 		return (
 			<div className="scorm-overview-body">
 				<div className="scorm-content">
-					{this.state.scormLink !== '' && <iframe className="scorm-container" src={this.state.scormLink} />}
 					{!isInstructor && this.renderStudent()}
 					{isInstructor && this.renderInstructor()}
 				</div>
