d65dba171a59db3664fdaee63389ca41f912e761
Jonathan Grimes <jsg2021@gmail.com>
2018-03-06 11:58:28 -0600

    test-harness: load all the overviews

--- test/app/index.js
+++ test/app/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {getService} from 'nti-web-client';
 import {Layouts} from 'nti-web-commons';
+import {Tasks} from 'nti-commons';
 
 import Overview from '../../src/overview/lesson/Overview';
 
@@ -17,6 +18,44 @@ import 'nti-web-whiteboard/lib/index.css';
 Layouts.Responsive.setWebappContext();
 
 window.$AppConfig = window.$AppConfig || {server: '/dataserver2/'};
+
+const queue = new Tasks.Executor();
+
+class Unit extends React.Component {
+	static propTypes = {
+		course: PropTypes.object,
+		layout: PropTypes.any,
+		node: PropTypes.object,
+	}
+
+	state = {}
+
+	async componentDidMount () {
+		const {node} = this.props;
+		const overview = await queue.queueTask(() => node.getContent());
+
+		this.setState({
+			overview
+		});
+	}
+
+	render () {
+		const {
+			props: {
+				course,
+				layout,
+				node
+			},
+			state: {
+				overview
+			}
+		} = this;
+
+		return !overview ? null : (
+			<Overview course={course} outlineNode={node} overview={overview} layout={layout}/>
+		);
+	}
+}
 
 class Test extends React.Component {
 	static propTypes = {
@@ -33,14 +72,11 @@ class Test extends React.Component {
 		const course = await service.getObject(courseId);
 		const outline = await course.getOutline();
 		const items = outline.getFlattenedList();
-		const contentItems = items.filter(item => item.hasLink('overview-content'));
-		const outlineNode = contentItems[0];
-		const overview = await outlineNode.getContent();
+		const nodes = items.filter(item => item.hasLink('overview-content'));
 
 		this.setState({
 			course,
-			outlineNode,
-			overview
+			nodes
 		});
 	}
 
@@ -60,7 +96,7 @@ class Test extends React.Component {
 
 
 	render () {
-		const {course, outlineNode, overview, layout} = this.state;
+		const {course, nodes, layout} = this.state;
 
 		if (!course) { return null; }
 
@@ -68,7 +104,10 @@ class Test extends React.Component {
 			<div>
 				<label><input type="radio" checked={layout === Overview.Grid} onChange={this.selectGrid}/>Grid</label>
 				<label><input type="radio" checked={layout === Overview.List} onChange={this.selectList}/>List</label>
-				<Overview course={course} outlineNode={outlineNode} overview={overview} layout={layout}/>
+
+				{nodes.map(node => (
+					<Unit key={node.NTIID} course={course} node={node} layout={layout}/>
+				))}
 			</div>
 
 		);
