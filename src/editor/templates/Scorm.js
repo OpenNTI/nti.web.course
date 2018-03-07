ceebca70fecbb620cc6fe379b21e8c75734f7644
Cory Jones <cory.jones@nextthought.com>
2018-03-07 16:57:16 -0600

    NTI-5218 Remove punctuation from scorm template item

--- src/editor/templates/Scorm.js
+++ src/editor/templates/Scorm.js
@@ -2,7 +2,7 @@ import { ScormBasic, ScormImport, ImportConfirmation } from '../panels';
 
 export const Scorm = {
 	name: 'Scorm',
-	description: 'Create a new scorm course by providing all of the course information.',
+	description: 'Create a new scorm course by providing all of the course information',
 
 	panels: [
 		ScormBasic,
