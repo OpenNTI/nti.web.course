8c5041eebae2ac8ba33260a80163f9a5c29fcb77
Andrew Ligon <andrew.ligon@nextthought.com>
2018-03-07 11:27:56 -0600

    Add the timeline grid item

--- src/overview/lesson/items/timeline/Grid.jsx
+++ src/overview/lesson/items/timeline/Grid.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Card} from 'nti-web-commons';
 
-export default function LessonOverviewTimelineGridItem () {
+import PaddedContainer from '../../common/PaddedContainer';
+
+LessonOverviewTimelineGridItem.propTypes = {
+	item: PropTypes.object.isRequired,
+	course: PropTypes.object.isRequired
+};
+export default function LessonOverviewTimelineGridItem ({item, course}) {
 	return (
-		<div>
-			Timeline Grid Item
-		</div>
+		<PaddedContainer>
+			<Card item={item} contentPackage={course} internalOverride/>
+		</PaddedContainer>
 	);
 }
 
