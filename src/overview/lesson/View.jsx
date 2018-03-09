16cf74c8f25a06e5b74e7a110f8a472c5cc7a6f0
Andrew Ligon <andrew.ligon@nextthought.com>
2018-03-09 14:36:04 -0600

    NTI-5254 NTI-5244 Style and remember the layout toggle

--- src/overview/lesson/View.jsx
+++ src/overview/lesson/View.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import Storage from 'nti-web-storage';
 
 import {Grid, List} from './Constants';
 import Overview from './Overview';
 import PaddedContainer from './common/PaddedContainer';
 
 const RADIO_GROUP = 'nti-lesson-view-layout';
+const STORAGE_KEY = 'nti-lesson-view-layout-value';
 
 export default class LessonView extends React.Component {
 	static propTypes = {
@@ -14,17 +16,19 @@ export default class LessonView extends React.Component {
 	}
 
 	state = {
-		layout: Grid
+		layout: Storage.getItem(STORAGE_KEY) || Grid
 	}
 
 
 	selectGrid = () => {
 		this.setState({layout: Grid});
+		Storage.setItem(STORAGE_KEY, Grid);
 	}
 
 
 	selectList = () => {
 		this.setState({layout: List});
+		Storage.setItem(STORAGE_KEY, List);
 	}
 
 
@@ -35,14 +39,21 @@ export default class LessonView extends React.Component {
 		return (
 			<div className={cx('nti-lesson-view', className)}>
 				<PaddedContainer className="header">
-					<label className="grid">
-						<input type="radio" group={RADIO_GROUP} checked={layout === Grid} onChange={this.selectGrid} />
-						<span>Grid</span>
-					</label>
-					<label className="list">
-						<input type="radio" group={RADIO_GROUP} checked={layout === List} onChange={this.selectList} />
-						<span>List</span>
-					</label>
+					<div className="spacer" />
+					<div className="layout-toggle">
+						<label className="grid">
+							<input type="radio" group={RADIO_GROUP} name="grid" checked={layout === Grid} onChange={this.selectGrid} />
+							<div className="toggle">
+								<i className="icon-grid" />
+							</div>
+						</label>
+						<label className="list">
+							<input type="radio" group={RADIO_GROUP} name="list" checked={layout === List} onChange={this.selectList} />
+							<div className="toggle">
+								<i className="icon-list" />
+							</div>
+						</label>
+					</div>
 				</PaddedContainer>
 				<Overview {...otherProps} layout={layout} />
 			</div>
