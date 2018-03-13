01aa6d12ce103579a7b30dcdc94ab1f4d24f3c0f
Cory Jones <cory.jones@nextthought.com>
2018-03-13 10:34:24 -0500

    NTI-5274 Export CourseMenu from course component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * as Editor from './editor';
 export Info from './info/inline/CourseInfo';
 export Progress from './progress/progress';
 export ProgressWindow from './progress/window';
+export CourseMenu from './listing/CourseMenu';
 export CourseItem from './info/Item';
 export Roster from './roster';
 export * as AdminTools from './admin-tools';
