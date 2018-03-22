6fb2e3920acfa51c9cf5fc7d155ee4dd520f67c4
Andrew Ligon <andrew.ligon@nextthought.com>
2018-03-22 16:58:50 -0500

    Style the page header

--- src/progress/overview/contents/Page.jsx
+++ src/progress/overview/contents/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import {DateTime} from 'nti-web-commons';
+import {DateTime} from 'nti-web-commons';
 
 import Overview from '../../../overview/lesson/OverviewContents';
 import PaddedContainer from '../../../overview/lesson/common/PaddedContainer';
@@ -46,6 +46,9 @@ export default class ProgressOverviewContentsPage extends React.Component {
 		const {loading, page, pageHeight, course} = this.props;
 		const overview = page && page.Items[0];
 		const outlineNode = overview && overview.parent();
+		const available = outlineNode && outlineNode.getAvailableBeginning();
+		const ending = outlineNode && outlineNode.getAvailableEnding();
+		const dateFormat = 'MMMM, D YYYY';
 
 		return (
 			<div className="progress-overview-contents-page">
@@ -53,6 +56,10 @@ export default class ProgressOverviewContentsPage extends React.Component {
 				{overview && overview.Items && overview.Items.length && (
 					<React.Fragment>
 						<PaddedContainer className="header">
+							<div className="sub-title">
+								{available && (<DateTime format={dateFormat} date={available} />)}
+								{!available && ending && (<DateTime format={dateFormat} date={ending} />)}
+							</div>
 							<div className="title">
 								{overview.title}
 							</div>
