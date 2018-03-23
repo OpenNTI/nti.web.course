7922334c121b5cba4f96e43537acc5d2667cc52a
Andrew Ligon <andrew.ligon@nextthought.com>
2018-03-22 22:47:51 -0500

    Checkpoint for a pager

--- src/progress/overview/Header.jsx
+++ src/progress/overview/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {DisplayName} from 'nti-web-commons';
+import {scoped} from 'nti-lib-locale';
+
+const t = scoped('course.progress.overview.Header', {
+	page: '%(page)s of %(total)s'
+});
 
 export default class ProgressOverviewHEader extends React.Component {
 	static propTypes = {
-		enrollment: PropTypes.object,
 		doClose: PropTypes.func,
 		singleItem: PropTypes.bool,
+
+		currentItem: PropTypes.object,
+		currentItemIndex: PropTypes.number,
+		totalItems: PropTypes.number,
 
 		hasNextItem: PropTypes.bool,
 		hasPrevItem: PropTypes.bool,
@@ -24,13 +34,31 @@ export default class ProgressOverviewHEader extends React.Component {
 	}
 
 
+	loadPrevItem = () => {
+		const {loadPrevItem} = this.props;
+
+		if (loadPrevItem) {
+			loadPrevItem();
+		}
+	}
+
+
+	loadNextItem = () => {
+		const {loadNextItem} = this.props;
+
+		if (loadNextItem) {
+			loadNextItem();
+		}
+	}
+
+
 	render () {
-		const {singleItem} = this.props;
+		const {singleItem, currentItem} = this.props;
 
 		return (
 			<div className="progress-overview-header">
-				{!singleItem && (this.renderPager())}
-				{singleItem && (<div className="spacer"/>)}
+				{!singleItem && currentItem && (this.renderPager())}
+				{singleItem && !currentItem && (<div className="spacer"/>)}
 				<div className="close" onClick={this.doClose}>
 					<i className="icon-light-x" />
 				</div>
@@ -40,6 +68,22 @@ export default class ProgressOverviewHEader extends React.Component {
 
 
 	renderPager () {
+		const {currentItem, totalItems, currentItemIndex, hasNextItem, hasPrevItem} = this.props;
 
+		return (
+			<React.Fragment>
+				<DisplayName entity={currentItem.UserProfile} />
+				<div className="spacer" />
+				<div className="current-page">
+					{t('page', {page: currentItemIndex || 0, total: totalItems || 0})}
+				</div>
+				<div className={cx('page-up', 'page-control', {disabled: !hasPrevItem})} onClick={this.loadPrevItem}>
+					<i className="icon-chevron-up" />
+				</div>
+				<div className={cx('page-down', 'page-control', {disabled: !hasNextItem})} onClick={this.loadNextItem}>
+					<i className="icon-chevron-down" />
+				</div>
+			</React.Fragment>
+		);
 	}
 }
--- src/progress/overview/Store.js
+++ src/progress/overview/Store.js
@@ -1,4 +1,5 @@
 import {Stores} from 'nti-lib-store';
+import {getService} from 'nti-web-client';
 
 export default class ProgressOverviewStore extends Stores.SimpleStore {
 	constructor () {
@@ -8,12 +9,38 @@ export default class ProgressOverviewStore extends Stores.SimpleStore {
 
 		this.set('totalItems', null);
 		this.set('currentItemIndex', null);
-		this.set('hasNextPage', false);
-		this.set('hasPrevPage', false);
+		this.set('hasNextItem', false);
+		this.set('hasPrevItem', false);
 	}
 
-	loadBatch (batch) {
 
+	async loadNextItem () {
+		const service = await getService();
+		const batch = await service.getBatch(this.nextLink);
+
+		this.loadBatch(batch);
+	}
+
+	async loadPrevItem () {
+		const service = await getService();
+		const batch = await service.getBatch(this.prevLink);
+
+		this.loadBatch(batch);
+	}
+
+
+	loadBatch (batch) {
+		const {Items, TotalItemCount, BatchPage} = batch;
+
+		this.batch = batch;
+		this.nextLink = batch.getLink('batch-next');
+		this.prevLink = batch.getLink('batch-prev');
+
+		this.set('currentItem', Items[0]);
+		this.set('currentItemIndex', BatchPage);
+		this.set('totalItems', TotalItemCount);
+		this.set('hasNextItem', !!this.nextLink);
+		this.set('hasPrevItem', !!this.prevLink);
 	}
 
 
@@ -21,7 +48,7 @@ export default class ProgressOverviewStore extends Stores.SimpleStore {
 		this.set('currentItem', enrollment);
 		this.set('currentItemIndex', 1);
 		this.set('totalItems', 1);
-		this.set('hasNextPage', false);
-		this.set('hasPrevPage', false);
+		this.set('hasNextItem', false);
+		this.set('hasPrevItem', false);
 	}
 }
