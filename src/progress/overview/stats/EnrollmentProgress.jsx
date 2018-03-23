f9e0293c53aa6bc3d51ea46be82c47d358eb9256
Andrew Ligon <andrew.ligon@nextthought.com>
2018-03-23 11:57:34 -0500

    Show the enrollments progress by the avatar

--- src/progress/overview/stats/EnrollmentProgress.jsx
+++ src/progress/overview/stats/EnrollmentProgress.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Avatar, DisplayName} from 'nti-web-commons';
+import {CircularProgress} from 'nti-web-charts';
 
 
 ProgressOverviewStatsEnrollment.propTypes = {
 	enrollment: PropTypes.object
 };
 export default function ProgressOverviewStatsEnrollment ({enrollment}) {
-	const {UserProfile} = enrollment;
+	const {UserProfile, CourseProgress} = enrollment;
+	const progress = Math.floor((CourseProgress.PercentageProgress || 0) * 100);
 
 	return (
 		<div className="progress-overview-stats-enrollment-progress">
 			<div className="enrollment-info">
 				<div className="avatar-container">
 					<Avatar entity={UserProfile} />
+					<CircularProgress value={progress} width={35} height={35} />
 				</div>
 				<DisplayName entity={UserProfile} />
 			</div>
