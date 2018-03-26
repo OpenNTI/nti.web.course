c36bb1c3a17965d50c752b2a950dc05b19f042e1
Cory Jones <cory.jones@nextthought.com>
2018-03-26 11:34:19 -0500

    NTI-5411 Add progress indicators on Scorm view

---
+++ src/scorm/Progress.jsx
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {CircularProgress} from 'nti-web-charts';
+import { scoped } from 'nti-lib-locale';
+import cx from 'classnames';
+
+const t = scoped('scorm.progress', {
+	classCompletion: 'Class Completion',
+	passRate: 'Pass Rate',
+	avgTime: 'Avg Time Spent',
+	passed: 'Passed',
+	failed: 'Bummer..',
+	passedMsg: 'Congratulations, you earned a satisfactory score.',
+	failedMsg: 'Unfortunately, you did not earn a satisfactory score.',
+	contactInstructor: 'Contact Instructor',
+	noData: 'No Data Yet',
+	noDataMsg: 'Data about your course will appear here.'
+});
+
+export default class Progress extends Component {
+	static propTypes = {
+		bundle: PropTypes.shape({
+			getScormCourse: PropTypes.func.isRequired,
+			getID: PropTypes.func.isRequired,
+			getLink: PropTypes.func.isRequired,
+			fetchLink: PropTypes.func.isRequired
+		}),
+		error: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+		onBundleUpdate: PropTypes.func.isRequired,
+		isAdmin: PropTypes.bool
+	}
+
+	state = {
+	}
+
+	componentDidMount () {
+		this.loadData(this.props.bundle);
+	}
+
+	async loadData (bundle) {
+		if(this.props.isAdmin) {
+			// load ProgressStats
+			const courseProgress = await bundle.fetchLink('ProgressStats');
+
+			this.setState({
+				pctProgress: courseProgress.PercentageProgress,
+				courseProgress
+			});
+		}
+		else {
+			// get from PreferredAccess
+			const {PreferredAccess} = bundle;
+
+			const courseProgress = PreferredAccess.CourseProgress;
+
+			const completedItem = (courseProgress || {}).CompletedItem;
+
+			const completedDate = courseProgress && courseProgress.getCompletedDate();
+			const isCompleted = Boolean(completedDate);
+
+			this.setState({
+				completedDate,
+				completedItem,
+				isCompleted
+			});
+		}
+	}
+
+
+	renderLabeledValue (label, value, className) {
+		const cls = cx('labeled-value', className);
+
+		return (
+			<div className={cls}>
+				<div className="label">{label}</div>
+				<div className="value">{value}</div>
+			</div>
+		);
+	}
+
+
+	renderAdmin () {
+		return (
+			<div className="admin-view">
+				{this.state.courseProgress && <CircularProgress value={this.state.pctProgress} showPercentSymbol/>}
+			</div>
+		);
+	}
+
+
+	renderNotCompleted () {
+		return (
+			<div className="not-completed">
+				<div className="title">{t('noData')}</div>
+				<div className="message">{t('noDataMsg')}</div>
+			</div>
+		);
+	}
+
+
+	renderCompletionStatus (passed) {
+		if(passed == null) {
+			return this.renderNotCompleted();
+		}
+
+		const className = passed ? 'passed' : 'failed';
+		const title = passed ? t('passed') : t('failed');
+		const message = passed ? t('passedMsg') : t('failedMsg');
+
+		const statusCls = cx('completion-status', { passed });
+
+		return (
+			<div className={statusCls}>
+				<div className={className}/>
+				<div className="title">{title}</div>
+				<div className="message">{message}</div>
+			</div>
+		);
+	}
+
+	renderStudent () {
+		// TODO: get success flag from course progress
+		const {completedItem} = this.state;
+
+		let passed = null;
+
+		if(completedItem) {
+			passed = completedItem.Success;
+		}
+
+		// should we support contact instructor in the future?
+		// {passed !== null && <a className="contact-instructor-button" href="">{t('contactInstructor')}</a>}
+
+		return (
+			<div className="student-view">
+				{this.renderCompletionStatus(passed)}
+			</div>
+		);
+	}
+
+
+	render () {
+		const {isAdmin} = this.props;
+
+		return (
+			<div className="scorm-progress">
+				<div className="progress-content">
+					{isAdmin ? this.renderAdmin() : this.renderStudent()}
+				</div>
+			</div>
+		);
+	}
+}
--- src/scorm/View.jsx
+++ src/scorm/View.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { scoped } from 'nti-lib-locale';
 
 import Editor from './Editor';
+import Progress from './Progress';
 
 const IMPORT_SCORM = 'ImportScorm';
 const { Responsive } = Layouts;
@@ -56,19 +57,27 @@ class Scorm extends Component {
 		return this.props.bundle.getLink('SCORMArchive');
 	}
 
+	isCompletableCourse () {
+		// drive this by CompletionPolicy or CourseProgress
+		return Object.keys(this.props.bundle).includes('CompletionPolicy');
+	}
+
 	renderInstructor = () => {
 		const { showEditor } = this.state;
 		const { bundle, error } = this.props;
 		const canLaunchCourse = bundle.Metadata.hasLink('LaunchSCORM');
 		return (
 			<div className="scorm-card scorm-instructor-card">
-				<div className="scorm-title">{bundle.title}</div>
-				{!Responsive.isMobile() && <a className="scorm-edit-link" onClick={this.editScorm}>{canLaunchCourse ? t('packageChange') : t('packageUpload')}</a>}
-				{canLaunchCourse &&  <a className="scorm-export-link" href={this.exportScorm()} download>{t('packageExport')}</a>}
-				<div className="scorm-desc">{t('scormDescription')}</div>
-				{error && <div className="scorm-error">{error}</div>}
-				{canLaunchCourse && <Button className="scorm-launch-button" href={this.getLaunchLink()} rel="external">{t('launch')}</Button>}
-				{showEditor && !Responsive.isMobile() && <Editor onDismiss={this.onDismiss} onFinish={this.onFinish} bundle={bundle} />}
+				<div className="course-info">
+					<div className="scorm-title">{bundle.title}</div>
+					{!Responsive.isMobile() && <a className="scorm-edit-link" onClick={this.editScorm}>{canLaunchCourse ? t('packageChange') : t('packageUpload')}</a>}
+					{canLaunchCourse &&  <a className="scorm-export-link" href={this.exportScorm()} download>{t('packageExport')}</a>}
+					<div className="scorm-desc">{t('scormDescription')}</div>
+					{error && <div className="scorm-error">{error}</div>}
+					{canLaunchCourse && <Button className="scorm-launch-button" href={this.getLaunchLink()} rel="external">{t('launch')}</Button>}
+					{showEditor && !Responsive.isMobile() && <Editor onDismiss={this.onDismiss} onFinish={this.onFinish} bundle={bundle} />}
+				</div>
+				{this.isCompletableCourse() && <Progress bundle={bundle} isAdmin/>}
 			</div>
 		);
 	}
@@ -79,10 +88,13 @@ class Scorm extends Component {
 		return (
 			<div className="scorm-card scorm-student-card">
 				<div className="scorm-student-progress">
-					<div className="scorm-title">{ bundle.title }</div>
-					<div className="scorm-desc">{t('scormDescription')}</div>
-					{error && <div className="scorm-error">{error}</div>}
-					{bundle.Metadata.hasLink('LaunchSCORM') && <Button className="scorm-launch-button" href={this.getLaunchLink()} rel="external">{t('launch')}</Button>}
+					<div className="course-info">
+						<div className="scorm-title">{ bundle.title }</div>
+						<div className="scorm-desc">{t('scormDescription')}</div>
+						{error && <div className="scorm-error">{error}</div>}
+						{bundle.Metadata.hasLink('LaunchSCORM') && <Button className="scorm-launch-button" href={this.getLaunchLink()} rel="external">{t('launch')}</Button>}
+					</div>
+					{this.isCompletableCourse() && <Progress bundle={bundle}/>}
 				</div>
 			</div>
 		);
