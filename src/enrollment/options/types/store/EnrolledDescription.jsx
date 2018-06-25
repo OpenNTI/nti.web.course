ab78486f2088b2ffef6dc421f442d8fa21f2f342
Andrew Ligon <andrew.ligon@nextthought.com>
2018-06-25 16:28:59 -0500

    fill in the enrolled state for store enrollment

---
+++ src/enrollment/options/types/store/EnrolledDescription.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {isArchived, getCatalogEntryData} from '../../utils';
+import Description from '../../common/Description';
+
+import t from './wording';
+
+export default class CourseEnrollmentStoreTypeEnrolledDescription extends React.Component {
+	static propTypes = {
+		catalogEntry: PropTypes.object
+	}
+
+	render () {
+		const {catalogEntry} = this.props;
+		const data = getCatalogEntryData(catalogEntry);
+
+		return (
+			<Description>
+				{isArchived(catalogEntry) ? this.renderArchived(data) : this.renderActive(data)}
+			</Description>
+		);
+	}
+
+
+	renderArchived (data) {
+		return t('enrolled.description.archived', data);
+	}
+
+
+	renderActive (data) {
+		return data.fullStartDate ?
+			t('enrolled.description.active.startDate', data) :
+			t('enrolled.description.active.noStartDate', data);
+	}
+}
