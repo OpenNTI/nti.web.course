64a9b7224aded28fc8d126b099ad0a300ec4c46c
Andrew Ligon <andrew.ligon@nextthought.com>
2018-06-25 16:28:59 -0500

    fill in the enrolled state for store enrollment

--- src/enrollment/options/types/store/Option.js
+++ src/enrollment/options/types/store/Option.js
@@ -1,6 +1,9 @@
 import Base from '../base';
 import Registry from '../Registry';
 
+import EnrolledTitle from './EnrolledTitle';
+import EnrolledDescription from './EnrolledDescription';
+
 function handles (option) {
 	return option.MimeType === 'application/vnd.nextthought.courseware.storeenrollmentoption';
 }
@@ -8,6 +11,9 @@ function handles (option) {
 @Registry.register(handles)
 export default class StoreEnrollmentOption extends Base {
 	ORDER = 2
+
+	EnrolledTitle = EnrolledTitle
+	EnrolledDescription = EnrolledDescription
 
 	async load (option) {
 		if (!this.isAvailable() && !this.isEnrolled()) {
