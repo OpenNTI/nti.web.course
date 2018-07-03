1a29c43c4598065d6c24770086e14ea0b4a282a7
Andrew Ligon <andrew.ligon@nextthought.com>
2018-07-03 11:16:02 -0500

    Style the upgrade buttons

---
+++ src/enrollment/options/common/EnrollmentLink.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {LinkTo} from '@nti/web-routing';
+
+export default class CourseOptionEnrollmentLink extends React.Component {
+	static propTypes = {
+		option: PropTypes.shape({
+			option: PropTypes.object.isRequired
+		}).isRequired
+	}
+
+	render () {
+		const {option, ...otherProps} = this.props;
+
+
+		return (
+			<LinkTo.Object {...otherProps} object={option.option} context="enroll" />
+		);
+	}
+}
