ea6693fd248046d13963e6642365d50724dd7fc2
Andrew Ligon <andrew.ligon@nextthought.com>
2018-07-05 14:33:56 -0500

    Update getting wording to not be as limited.

--- src/enrollment/options/utils/index.js
+++ src/enrollment/options/utils/index.js
@@ -1,3 +1,5 @@
 export getCatalogEntryData from './get-catalog-entry-data';
+export getTranslationFor from './get-translation-for';
+export hasStarted from './has-started';
 export isArchived from './is-archived';
 export sortOptions from './sort-options';
