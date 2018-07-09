83fcc73040a95c1c27b7ed8235464cc48b6a5a25
Cory Jones <cory.jones@nextthought.com>
2018-07-09 16:18:46 -0500

    More work on position select

--- src/overview/lesson/common/PositionSelect.jsx
+++ src/overview/lesson/common/PositionSelect.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Flyout, Input, DialogButtons} from '@nti/web-commons';
+import {getService} from '@nti/web-client';
 import cx from 'classnames';
 import {scoped} from '@nti/lib-locale';
 
@@ -9,6 +10,7 @@ const t = scoped('course.overview.lesson.common.PositionSelect', {
 	cancel: 'Cancel'
 });
 
+// TODO: belongs in OverviewGroup model or somewhere else?
 const COLOR_CHOICES = [
 	'F9824E',
 	'F5D420',
@@ -29,7 +31,9 @@ const COLOR_CHOICES = [
 ];
 
 export default class LessonOverviewPositionSelect extends React.Component {
-	attachFlyoutRef = x => this.flyout = x
+	attachSectionFlyoutRef = x => this.sectionFlyout = x
+
+	attachRankFlyoutRef = x => this.rankFlyout = x
 
 	static propTypes = {
 		lessonOverview: PropTypes.object.isRequired
@@ -38,7 +42,7 @@ export default class LessonOverviewPositionSelect extends React.Component {
 	state = {}
 
 	componentDidMount () {
-		this.setState({selectedSection: this.props.lessonOverview.Items[0]});
+		this.setState({selectedRank: 1, selectedSection: this.props.lessonOverview.Items[0]});
 	}
 
 	renderSectionInfo (section) {
@@ -51,7 +55,7 @@ export default class LessonOverviewPositionSelect extends React.Component {
 	}
 
 	renderSectionTrigger () {
-		return <div className="section-select">{this.renderSectionInfo(this.state.selectedSection || {})}</div>;
+		return <div className="section-trigger">{this.renderSectionInfo(this.state.selectedSection || {})}</div>;
 	}
 
 	renderSectionOption = (section) => {
@@ -60,12 +64,12 @@ export default class LessonOverviewPositionSelect extends React.Component {
 		return (
 			<div
 				className={className}
-				key={section.title}
+				key={section.getID()}
 				onClick={() => {
 					this.setState({selectedSection: section});
 
-					if(this.flyout) {
-						this.flyout.dismiss();
+					if(this.sectionFlyout) {
+						this.sectionFlyout.dismiss();
 					}
 				}}
 			>
@@ -82,8 +86,43 @@ export default class LessonOverviewPositionSelect extends React.Component {
 		this.setState({inCreationMode: false, hexValue: null, sectionName: null});
 	}
 
-	onSave = () => {
-		this.setState({inCreationMode: false});
+	onSave = async () => {
+		const {lessonOverview} = this.props;
+		const {sectionName, hexValue} = this.state;
+
+		if(!sectionName) {
+			this.setState({error: 'Title is required'});
+			return;
+		}
+
+		if(!hexValue) {
+			this.setState({error: 'Color is required'});
+			return;
+		}
+
+		try {
+			const contentsLink = lessonOverview.getLink('ordered-contents');
+
+			if(!contentsLink) {
+				this.setState({error: 'No contents link'});
+				return;
+			}
+
+			const content = {
+				MimeType: 'application/vnd.nextthought.nticourseoverviewgroup',
+				accentColor: hexValue,
+				title: sectionName
+			};
+
+			const service = await getService();
+			const newSection = await service.postParseResponse(contentsLink, content);
+			await lessonOverview.refresh();
+
+			this.setState({selectedSection: newSection, inCreationMode: false, hexValue: null, sectionName: null});
+		}
+		catch (e) {
+			this.setState({error: e.message || e});
+		}
 	}
 
 	sectionNameChange = (val) => {
@@ -107,8 +146,11 @@ export default class LessonOverviewPositionSelect extends React.Component {
 	}
 
 	renderCreateNewSection () {
+		const {error} = this.state;
+
 		return (
 			<div className="create-section-form">
+				{error && <div className="error">{error}</div>}
 				<div className="contents">
 					<Input.Text className="name-input" value={this.state.sectionName} onChange={this.sectionNameChange} placeholder="Section Name"/>
 					<div className="label">Choose a color</div>
@@ -136,11 +178,11 @@ export default class LessonOverviewPositionSelect extends React.Component {
 	renderSectionSelect () {
 		return (
 			<Flyout.Triggered
-				className="sync-options"
+				className="section-select"
 				trigger={this.renderSectionTrigger()}
 				horizontalAlign={Flyout.ALIGNMENTS.LEFT}
 				sizing={Flyout.SIZES.MATCH_SIDE}
-				ref={this.attachFlyoutRef}
+				ref={this.attachSectionFlyoutRef}
 			>
 				<div className="section-select-flyout">
 					{!this.state.inCreationMode && this.props.lessonOverview.Items.map(this.renderSectionOption)}
@@ -151,7 +193,63 @@ export default class LessonOverviewPositionSelect extends React.Component {
 		);
 	}
 
+	renderRankTrigger (disabled) {
+		const className = cx('rank-trigger', {disabled});
+
+		return <div className={className}>{this.state.selectedRank}</div>;
+	}
+
+	renderRankOption = (rank) => {
+		return (
+			<li
+				key={rank}
+				onClick={() => {
+					this.setState({selectedRank: rank + 1});
+
+					if(this.rankFlyout) {
+						this.rankFlyout.dismiss();
+					}
+				}}
+				className="rank-option"
+			>
+				{rank + 1}
+			</li>
+		);
+	}
+
+	renderRankSelect () {
+		const {selectedSection} = this.state;
+
+		if(!selectedSection) {
+			return null;
+		}
+
+		const {Items} = selectedSection;
+
+		if(!Items || Items.length === 0) {
+			return this.renderRankTrigger(true);
+		}
+
+		let availableRanks = Items.map((val, i) => i);
+
+		availableRanks.push(availableRanks.length);
+
+		return (
+			<Flyout.Triggered
+				className="rank-select"
+				trigger={this.renderRankTrigger()}
+				horizontalAlign={Flyout.ALIGNMENTS.RIGHT}
+				sizing={Flyout.SIZES.MATCH_SIDE}
+				ref={this.attachRankFlyoutRef}
+			>
+				<ul className="rank-select-flyout">
+					{availableRanks.map(this.renderRankOption)}
+				</ul>
+			</Flyout.Triggered>
+		);
+	}
+
 	render () {
-		return <div className="overview-position-select">{this.renderSectionSelect()}</div>;
+		return <div className="overview-position-select">{this.renderSectionSelect()}{this.renderRankSelect()}</div>;
 	}
 }
