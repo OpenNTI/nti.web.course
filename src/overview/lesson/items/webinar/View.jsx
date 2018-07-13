a9f2f1e94bc3eb1277e70a1291ab6c78d0e8941d
Jonathan Grimes <jsg2021@gmail.com>
2018-07-13 10:32:51 -0500

    move webinar course overview item into course

---
+++ src/overview/lesson/items/webinar/View.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { List } from '../../Constants';
+import Registry from '../Registry';
+
+export default
+@Registry.register('application/vnd.nextthought.webinar')
+class OverviewItemView extends React.Component {
+
+	static propTypes = {
+		item: PropTypes.object,
+		layout: PropTypes.any
+	}
+
+	render () {
+		const { layout } = this.props;
+
+		const minimal = layout === List;
+
+		return (
+			<div>
+				Webinar {minimal}
+			</div>
+		);
+	}
+}
