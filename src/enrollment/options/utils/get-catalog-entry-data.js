e85536743f3c6dc3dba9ab8feb16a1082b52c25e
Josh Birdwell <jbirdwell34@gmail.com>
2018-07-16 09:35:41 -0500

    be more defensive for admins who don't have options

--- src/enrollment/options/utils/get-catalog-entry-data.js
+++ src/enrollment/options/utils/get-catalog-entry-data.js
@@ -6,13 +6,13 @@ const M_D_Y_FORMAT = 'MMMM Do, YYYY';
 export default function getCatalogEntryData (catalogEntry, option) {
 	const startDate = catalogEntry.getStartDate();
 	const endDate = catalogEntry.getEndDate();
-	const enrollCutOffDate = option.getEnrollCutOffDate && option.getEnrollCutOffDate();
+	const enrollCutOffDate = option && option.getEnrollCutOffDate && option.getEnrollCutOffDate();
 
 	return {
 		fullStartDate: startDate && DateTime.format(startDate, FULL_FORMAT),
 		fullEndDate: endDate && DateTime.format(endDate, FULL_FORMAT),
 		title: catalogEntry.title,
-		price: option.getPrice && option.getPrice(),
+		price: option && option.getPrice && option.getPrice(),
 		enrollCutOffDate: enrollCutOffDate && DateTime.format(enrollCutOffDate, M_D_Y_FORMAT)
 	};
 }
