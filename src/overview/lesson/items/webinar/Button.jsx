ab75dde79794403451d1375517b7a59fdfe957d9
Cory Jones <cory.jones@nextthought.com>
2018-07-31 10:13:39 -0500

    Move state logic into store

--- src/overview/lesson/items/webinar/Button.jsx
+++ src/overview/lesson/items/webinar/Button.jsx
@@ -3,25 +3,16 @@ import PropTypes from 'prop-types';
 import {Timer, DateTime, Layouts} from '@nti/web-commons';
 import {GotoWebinar} from '@nti/web-integrations';
 import {scoped} from '@nti/lib-locale';
+import cx from 'classnames';
+
+import StateStore, {States} from './Store';
 
 const t = scoped('course.overview.lesson.items.webinar.Button', {
 	register: 'Register',
-	unregister: 'Un-Register',
 	join: 'Join',
-	starting: 'Starting'
+	starting: 'Starting',
+	expiresIn: 'Expires in %(timeLeft)s'
 });
-
-const States = {
-	Unregistered: 'unregistered',
-	UnregisteredStartingSoon: 'unregistered-starting-soon',
-	UnregisteredStartingInMinute: 'unregistered-starting-in-minute',
-	RegisteredInactive: 'registered-inactive',
-	RegisteredStartingSoon: 'registered-starting-soon',
-	RegisteredStartingInMinute: 'registered-starting-in-minute',
-	RegisteredActive: 'registered-active',
-	RegisteredExpiringSoon: 'registered-expiring-soon',
-	Expired: 'expired'
-};
 
 const UnregisteredStates = new Set([
 	States.Unregistered,
@@ -29,241 +20,91 @@ const UnregisteredStates = new Set([
 	States.UnregisteredStartingInMinute
 ]);
 
-const COUNTDOWN_THRESHOLD = 1000 * 60 * 60; // one hour
-const MINUTE_THRESHOLD = 1000 * 60;
 const BUTTON_TRANSITION_TIME = 3000;
 
-export default class Button extends React.Component {
+export default
+@StateStore.connect({
+	currentState: 'currentState',
+	remainingTime: 'remainingTime'
+})
+class Button extends React.Component {
 
 	static propTypes = {
 		item: PropTypes.object.isRequired,
-		onStatusChange: PropTypes.func
+		onStatusChange: PropTypes.func,
+		store: PropTypes.object,
+		remainingTime: PropTypes.number,
+		currentState: PropTypes.string
 	}
 
 	state = {}
 
+
 	componentDidMount () {
-		this.calculateState();
-	}
+		const {store, onStatusChange, item: {webinar}} = this.props;
 
-
-	calculateState (props = this.props) {
-		const {item: {webinar}, onStatusChange} = props;
-
-		let newState = null;
-
-		if(webinar && !webinar.isExpired() && !webinar.isJoinable() && webinar.hasLink('WebinarRegistrationFields')) {
-			// the webinar hasn't expired and has registration fields, meaning we it's available for registration
-			newState = this.handleUnregisteredStates(props);
-		}
-		else {
-			newState = this.handleRegisteredStates(props);
-		}
-
-		if(newState) {
-			this.setState(newState, () => {
-				if(onStatusChange) {
-					onStatusChange(this.state.currentState);
-				}
-			});
-		}
-	}
-
-	// only allow one timeout any given time
-	schedule (fn, timeout) {
-		clearTimeout(this.timeout);
-
-		this.timeout = setTimeout(fn, timeout);
-	}
-
-	handleRegisteredStates (props) {
-		//TODO: We should find a way to simplify this.
-		const {item: {webinar}} = props;
-
-		const nearestSession = webinar.getNearestSession();
-		const now = Date.now();
-		let newState = null;
-
-		if(nearestSession.getStartTime() > now && (nearestSession.getStartTime() - MINUTE_THRESHOLD) < now) {
-			// the webinar hasn't started and is within the threshold of showing a 'Starting' label
-			newState = {
-				currentState: States.RegisteredStartingInMinute
-			};
-
-			// after the minute threshold has passed before starting, trigger a recalculation
-			this.schedule(() => {
-				this.calculateState();
-			}, Math.min(MINUTE_THRESHOLD, nearestSession.getStartTime() - now));
-		}
-		else if(nearestSession.getStartTime() > now && (nearestSession.getStartTime() - COUNTDOWN_THRESHOLD) >= now) {
-			// the webinar hasn't started and we're still too far out of the range for showing the countdown
-			newState = {
-				currentState: States.RegisteredInactive
-			};
-
-			const timeoutLength = nearestSession.getStartTime() - COUNTDOWN_THRESHOLD - now;
-
-			// when the right amount of time has passed, recalculate the state as we should move us to the registered starting soon state
-			this.schedule(() => {
-				this.calculateState();
-			}, timeoutLength);
-		}
-		else if(nearestSession.getStartTime() > now && (nearestSession.getStartTime() - COUNTDOWN_THRESHOLD) < now) {
-			// the webinar hasn't started but we're within the countdown threshold, so show the countdown timer on the button
-			newState = {
-				currentState: States.RegisteredStartingSoon,
-				remainingTime: nearestSession.getStartTime() - now
-			};
-		}
-		else if(nearestSession.getEndTime() < now) {
-			// the webinar has expired
-			newState = {
-				currentState: States.Expired
-			};
-		}
-		else if(nearestSession.getEndTime() - COUNTDOWN_THRESHOLD < now) {
-			// the webinar's end time is within the countdown threshold, so show the countdown to expiration timer on the button
-			newState = {
-				currentState: States.RegisteredExpiringSoon,
-				remainingTime: nearestSession.getEndTime() - now
-			};
-		}
-		else if(nearestSession.getStartTime() < now && (nearestSession.getEndTime() - COUNTDOWN_THRESHOLD) > now) {
-			// the webinar's end time is still too far out of the range for showing the countdown, so just show a 'Join' label in the meantime
-			newState = {
-				currentState: States.RegisteredActive
-			};
-
-			const timeoutLength = nearestSession.getEndTime() - COUNTDOWN_THRESHOLD - now;
-
-			// when the right amount of time has passed, recalculate the state as we should move us to the registered expiring soon state
-			this.schedule(() => {
-				this.calculateState();
-			}, timeoutLength);
-		}
-
-		return newState;
-	}
-
-
-	handleUnregisteredStates (props) {
-		//TODO: We should find a way to simplify this.
-		const {item: {webinar}} = props;
-
-		const nearestSession = webinar.getNearestSession();
-		const now = Date.now();
-
-		let newState = null;
-
-		// the webinar hasn't expired and has registration fields, meaning we it's available for registration
-		if (nearestSession.getStartTime() > now && (nearestSession.getStartTime() - MINUTE_THRESHOLD) < now) {
-			// the webinar is starting within the minute threshold, so show a Register/Starting message
-			newState = {
-				currentState: States.UnregisteredStartingInMinute
-			};
-
-			//FIXME: all timeouts in this component should be tracked and canceled before new timeouts are issued.
-
-			// after the minute threshold has passed before starting, trigger a recalculation
-			this.schedule(() => {
-				this.calculateState();
-			}, Math.min(MINUTE_THRESHOLD, nearestSession.getStartTime() - now));
-		}
-		else if(nearestSession.getStartTime() > now && (nearestSession.getStartTime() - COUNTDOWN_THRESHOLD) < now) {
-			// the webinar will start soon, show timer and Register label
-			newState = {
-				currentState: States.UnregisteredStartingSoon,
-				remainingTime: nearestSession.getStartTime() - now
-			};
-		}
-		else {
-			// the webinar will not start soon, just show Register label
-			newState = {
-				currentState: States.Unregistered
-			};
-
-			const timeoutLength = nearestSession.getStartTime() - COUNTDOWN_THRESHOLD - now;
-
-			// when the right amount of time has passed, recalculate the state as we should move us to the unregistered starting soon state
-			this.schedule(() => {
-				this.calculateState();
-			}, timeoutLength);
-		}
-
-		return newState;
+		store.calculateState(webinar, onStatusChange);
 	}
 
 
 	onTick = (clock) => {
-		const {item:{webinar}} = this.props;
-		const {currentState} = this.state;
+		const {store} = this.props;
 
-		const targetTime = currentState === States.RegisteredStartingSoon || currentState === States.UnregisteredStartingSoon
-			? webinar.getNearestSession().getStartTime()
-			: webinar.getNearestSession().getEndTime();
-
-		const remainingTime = targetTime - clock.current;
-
-		if(remainingTime <= 0
-				|| (currentState === States.RegisteredStartingSoon
-				|| currentState === States.UnregisteredStartingSoon
-				&& remainingTime <= MINUTE_THRESHOLD)) {
-			this.calculateState();
-		}
-		else {
-			this.setState({remainingTime: targetTime - clock.current});
-		}
+		store.onTick(clock);
 	}
 
 
 	renderJoinContents (enabled) {
-		const {currentState} = this.state;
+		const {currentState, remainingTime} = this.props;
 
-		//TODO: We should find a way to simplify this.
-		if(currentState === States.RegisteredActive || currentState === States.RegisteredInactive) {
-			return (
-				<button className="join" disabled={!enabled}>
-					<span>{t('join')}</span>
-				</button>
+		let buttonContents = (<span>{t('join')}</span>);
+		let additionalCls = null;
+
+		if(currentState === States.RegisteredStartingSoon) {
+			additionalCls = 'starting';
+
+			buttonContents = (
+				<Layouts.Carousel transition={Layouts.Carousel.ROTATE_UP} interval={BUTTON_TRANSITION_TIME} active={0} wrap>
+					<Timer onTick={this.onTick}>
+						<div>
+							<span className="timer"/>
+							<span className="remaining">{DateTime.getShortNaturalDuration(remainingTime)}</span>
+						</div>
+					</Timer>
+					<div>{t('join')}</div>
+				</Layouts.Carousel>
 			);
 		}
-		else if(currentState === States.RegisteredStartingSoon || currentState === States.RegisteredStartingInMinute) {
-			return (
-				<button className="join starting" disabled={!enabled}>
-					{currentState === States.RegisteredStartingSoon && (
-						<Layouts.Carousel transition={Layouts.Carousel.ROTATE_UP} interval={BUTTON_TRANSITION_TIME} active={0} wrap>
-							<Timer onTick={this.onTick}>
-								<div>
-									<span className="timer"/>
-									<span className="remaining">{DateTime.getShortNaturalDuration(this.state.remainingTime)}</span>
-								</div>
-							</Timer>
-							<div>{t('join')}</div>
-						</Layouts.Carousel>
-					)}
-					{currentState === States.RegisteredStartingInMinute && (
-						<Layouts.Carousel transition={Layouts.Carousel.ROTATE_UP} interval={BUTTON_TRANSITION_TIME} active={0} wrap>
-							<div>{t('starting')}</div>
-							<div>{t('join')}</div>
-						</Layouts.Carousel>
-					)}
-				</button>
+		else if(currentState === States.RegisteredStartingInMinute) {
+			additionalCls = 'starting';
+
+			buttonContents = (
+				<Layouts.Carousel transition={Layouts.Carousel.ROTATE_UP} interval={BUTTON_TRANSITION_TIME} active={0} wrap>
+					<div>{t('starting')}</div>
+					<div>{t('join')}</div>
+				</Layouts.Carousel>
 			);
 		}
 		else if(currentState === States.RegisteredExpiringSoon) {
-			return (
-				<button className="join expiring" disabled={!enabled}>
-					<Timer onTick={this.onTick}>
-						<div>
-							<div>{t('join')}</div>
-							<div className="remaining">
-								Expires in {DateTime.formatDuration(this.state.remainingTime / 1000)}
-							</div>
+			additionalCls = 'expiring';
+
+			buttonContents = (
+				<Timer onTick={this.onTick}>
+					<div>
+						<div>{t('join')}</div>
+						<div className="remaining">
+							{t('expiresIn', {timeLeft: DateTime.formatDuration(remainingTime / 1000)})}
 						</div>
-					</Timer>
-				</button>
+					</div>
+				</Timer>
 			);
 		}
+
+		return (
+			<button className={cx('join', additionalCls)} disabled={!enabled}>
+				{buttonContents}
+			</button>
+		);
 	}
 
 
@@ -286,10 +127,10 @@ export default class Button extends React.Component {
 		const open = () => toggle(true);
 		const close = () => {
 			toggle(false);
-			this.calculateState();
+			this.props.store.calculateState();
 		};
 
-		const {currentState} = this.state;
+		const {currentState, remainingTime} = this.props;
 
 		let buttonLabel = t('register');
 
@@ -300,7 +141,7 @@ export default class Button extends React.Component {
 						<div className="register-timer">
 							<span className="timer"/>
 							<span className="remaining">
-								{DateTime.getShortNaturalDuration(this.state.remainingTime)}
+								{DateTime.getShortNaturalDuration(remainingTime)}
 							</span>
 						</div>
 					</Timer>
@@ -330,7 +171,7 @@ export default class Button extends React.Component {
 
 
 	render () {
-		const {currentState} = this.state;
+		const {currentState} = this.props;
 
 		if(UnregisteredStates.has(currentState)) {
 			return this.renderRegisterButton();
---
+++ src/overview/lesson/items/webinar/Store.js
@@ -0,0 +1,131 @@
+import {Stores} from '@nti/lib-store';
+
+const COUNTDOWN_THRESHOLD = 1000 * 7; // one hour
+const MINUTE_THRESHOLD = 1000 * 4;
+
+export const States = {
+	Unregistered: 'unregistered',
+	UnregisteredStartingSoon: 'unregistered-starting-soon',
+	UnregisteredStartingInMinute: 'unregistered-starting-in-minute',
+	RegisteredInactive: 'registered-inactive',
+	RegisteredStartingSoon: 'registered-starting-soon',
+	RegisteredStartingInMinute: 'registered-starting-in-minute',
+	RegisteredActive: 'registered-active',
+	RegisteredExpiringSoon: 'registered-expiring-soon',
+	Expired: 'expired'
+};
+
+export default class WebinarStateStore extends Stores.SimpleStore {
+	onTick = (clock) => {
+		const currentState = this.get('currentState');
+
+		const targetTime = currentState === States.RegisteredStartingSoon || currentState === States.UnregisteredStartingSoon
+			? this.webinar.getNearestSession().getStartTime()
+			: this.webinar.getNearestSession().getEndTime();
+
+		const remainingTime = targetTime - clock.current;
+		const needsToRecalculate = currentState === (States.RegisteredStartingSoon || currentState === States.UnregisteredStartingSoon)
+			&& remainingTime <= MINUTE_THRESHOLD;
+
+		if(remainingTime <= 0 || needsToRecalculate) {
+			this.calculateState();
+		}
+		else {
+			this.set('remainingTime',  targetTime - clock.current);
+
+			this.emitChange('remainingTime');
+		}
+	}
+
+	// only allow one timeout any given time
+	schedule (timeout) {
+		clearTimeout(this.timeout);
+
+		this.timeout = setTimeout(() => { this.calculateState(); }, timeout);
+	}
+
+	isSessionStartingWithinMinute (session, now) {
+		return session.getStartTime() > now && (session.getStartTime() - MINUTE_THRESHOLD) < now;
+	}
+
+	isSessionStartingInMoreThanAMinute (session, now) {
+		return session.getStartTime() > now && (session.getStartTime() - COUNTDOWN_THRESHOLD) < now;
+	}
+
+	isSessionFarFromStarting (session, now) {
+		return session.getStartTime() > now && (session.getStartTime() - COUNTDOWN_THRESHOLD) >= now;
+	}
+
+	isSessionExpiringSoon (session, now) {
+		return session.getEndTime() - COUNTDOWN_THRESHOLD <= now;
+	}
+
+	isSessionActive (session, now) {
+		return session.getStartTime() < now && (session.getEndTime() - COUNTDOWN_THRESHOLD) >= now;
+	}
+
+	calculateState (webinar, onStatusChange) {
+		let newState = null;
+
+		if(webinar) {
+			this.webinar = webinar;
+		}
+
+		if(onStatusChange) {
+			this.onStatusChange = onStatusChange;
+		}
+
+		const nearestSession = this.webinar.getNearestSession();
+		const now = Date.now();
+		const isUnregistered = this.webinar && !this.webinar.isExpired() && !this.webinar.isJoinable() && this.webinar.hasLink('WebinarRegistrationFields');
+
+		if(this.webinar.isExpired()) {
+			newState = { currentState: States.Expired };
+		}
+		else if(this.isSessionStartingWithinMinute(nearestSession, now)) {
+			newState = { currentState: isUnregistered ? States.UnregisteredStartingInMinute : States.RegisteredStartingInMinute };
+
+			this.schedule(Math.min(MINUTE_THRESHOLD, nearestSession.getStartTime() - now)); // recalculate when we expect the webinar to start
+		}
+		else if(this.isSessionFarFromStarting(nearestSession, now)) {
+			newState = { currentState: isUnregistered ? States.Unregistered : States.RegisteredInactive };
+
+			this.schedule(nearestSession.getStartTime() - COUNTDOWN_THRESHOLD - now); // recalculate when we get to the countdown threshold
+		}
+		else if(this.isSessionStartingInMoreThanAMinute(nearestSession, now)) {
+			newState = {
+				currentState: isUnregistered ? States.UnregisteredStartingSoon : States.RegisteredStartingSoon,
+				remainingTime: nearestSession.getStartTime() - now
+			};
+		}
+		else if(this.isSessionExpiringSoon(nearestSession, now)) {
+			newState = {
+				currentState: isUnregistered ? States.Unregistered : States.RegisteredExpiringSoon,
+				remainingTime: nearestSession.getEndTime() - now
+			};
+
+			if(isUnregistered) {
+				this.schedule(newState.remainingTime); // unregistered doesn't have an expiration countdown, starting counting down now
+			}
+		}
+		else if(this.isSessionActive(nearestSession, now)) {
+			newState = { currentState: isUnregistered ? States.Unregistered : States.RegisteredActive };
+
+			this.schedule(nearestSession.getEndTime() - COUNTDOWN_THRESHOLD - now); // recalculate when we hit the expiration countdown threshold
+		}
+		else if(isUnregistered) {
+			newState = { currentState: States.Unregistered };
+		}
+
+		if(newState) {
+			if(this.onStatusChange) {
+				this.onStatusChange(newState.currentState);
+			}
+
+			this.set('currentState', newState.currentState);
+			this.set('remainingTime', newState.remainingTime);
+
+			this.emitChange('currentState', 'remainingTime');
+		}
+	}
+}
