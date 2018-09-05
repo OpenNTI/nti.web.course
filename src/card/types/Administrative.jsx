dd81b51d3debaae063ed631a50fa696bc10dd361
Landon Sherwood <landonsherwood23@gmail.com>
2018-09-05 11:59:01 -0500

    Add settings menu for admin card

--- src/card/types/Administrative.jsx
+++ src/card/types/Administrative.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {scoped} from '@nti/lib-locale';
 import {LinkTo} from '@nti/web-routing';
-import {DateTime} from '@nti/web-commons';
+import { getService } from '@nti/web-client';
+import {DateTime, Prompt, Flyout} from '@nti/web-commons';
 
 import {getSemesterBadge} from '../../utils/Semester';
 import Card from '../parts/Card';
 import Badge from '../parts/Badge';
+import CourseMenu from '../parts/CourseSettingsMenu';
 
 import Registry from './Registry';
 
-const t = scoped('course.card.type.sdministering', {
-	starting: 'preview'
+const t = scoped('course.card.type.administering', {
+	starting: 'preview',
+	confirmDelete: 'Do you want to delete this course?'
 });
 
 @Registry.register('application/vnd.nextthought.courseware.courseinstanceadministrativerole')
@@ -20,10 +23,76 @@ export default class Administrative extends React.Component {
 		course: PropTypes.object.isRequired
 	}
 
+	attachOptionsFlyoutRef = x => this.optionsFlyout = x
+
+	deleteCourse = (e) => {
+		const { course, onModification } = this.props;
+
+		e.stopPropagation();
+		e.preventDefault();
+
+		Prompt.areYouSure(t('confirmDelete')).then(() => {
+			this.setState( { loading: true }, () => {
+				getService().then((service) => {
+					return service.getObject(course.CatalogEntry.CourseNTIID).then((courseInstance) => {
+						return courseInstance.delete();
+					});
+				}).then(() => {
+					onModification && onModification();
+				}).catch((err) => {
+					console.error(err); //eslint-disable-line
+					// timeout here because there is a 500 ms delay on the areYouSure dialog being dismissed
+					// so if the deletion fails too fast, we risk automatically dismissing this alert dialog
+					// when the areYouSure dialog is dismissed
+					setTimeout(() => {
+						Prompt.alert('You don\'t have permission to delete this course');
+					}, 505);
+				});
+			});
+		});
+	};
+
+	doRequestSupport = () => {
+		global.location.href = 'mailto:support@nextthought.com?subject=Support%20Request';
+	}
+
+	doEdit = () => {
+		const { onEdit, course } = this.props;
+
+		onEdit && this.context.router.routeTo.object(course);
+	}
+
+	doExport = () => {
+		const { course } = this.props;
+
+		if(course.CatalogEntry.hasLink('Export')) {
+			this.optionsFlyout && this.optionsFlyout.dismiss();
+
+			global.location.href = course.CatalogEntry.getLink('Export');
+		}
+	}
+
+	renderOptionsButton () {
+		return (<div className="nti-course-card-badge black settings"><i className="icon-settings"/></div>);
+	}
+
+	renderOptions () {
+		return (
+			<Flyout.Triggered
+				className="admin-course-options"
+				trigger={this.renderOptionsButton()}
+				horizontalAlign={Flyout.ALIGNMENTS.RIGHT}
+				ref={this.attachOptionsFlyoutRef}
+			>
+				<CourseMenu course={this.props.course} doEdit={this.doEdit} doExport={this.doExport} doDelete={this.deleteCourse} doRequestSupport={this.doRequestSupport} registered />
+			</Flyout.Triggered>
+		);
+	}
+
 	render () {
 		const {course, ...otherProps} = this.props;
-		const startDate = course.getStartDate();
-		const endDate = course.getEndDate();
+		const startDate = course.CatalogEntry.getStartDate();
+		const endDate = course.CatalogEntry.getEndDate();
 		const preview = course.CatalogEntry.Preview;
 		const now = new Date();
 		const badges = [];
@@ -54,12 +123,6 @@ export default class Administrative extends React.Component {
 			));
 		}
 
-		badges.push((
-			<Badge black settings>
-				<i className="icon-settings" />
-			</Badge>
-		));
-
 		return (
 			<LinkTo.Object object={course}>
 				<Card
@@ -67,6 +130,7 @@ export default class Administrative extends React.Component {
 					course={course.CatalogEntry}
 					badges={badges}
 				/>
+				{this.renderOptions()}
 			</LinkTo.Object>
 		);
 	}
