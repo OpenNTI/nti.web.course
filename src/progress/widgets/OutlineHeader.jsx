6d70a21fe66d715d1106fade218ebb1879d73e2c
Cory Jones <cory.jones@nextthought.com>
2019-01-03 09:31:24 -0600

    First pass at pass/fail message under outline header

--- src/progress/widgets/OutlineHeader.jsx
+++ src/progress/widgets/OutlineHeader.jsx
@@ -5,8 +5,11 @@ import {CircularProgress} from '@nti/web-charts';
 import {Hooks} from '@nti/web-session';
 import {HOC, Iframe, Prompt} from '@nti/web-commons';
 import {getService} from '@nti/web-client';
+import cx from 'classnames';
 
-const t = scoped('course.components.GradeCard', {
+import PassFailMessage from './PassFailMessage';
+
+const t = scoped('course.progress.widgets.OutlineHeader', {
 	courseProgress: 'Course Progress',
 	outline: 'Outline',
 	getCertificate: 'View Certificate',
@@ -102,7 +105,8 @@ class OutlineHeader extends React.Component {
 			...this.getStateValues(courseProgress),
 			completedDate,
 			isCompleted,
-			type: STUDENT
+			type: STUDENT,
+			requirementsMet: false	// TODO: Determine this from course
 		});
 	}
 
@@ -213,6 +217,14 @@ class OutlineHeader extends React.Component {
 		);
 	}
 
+	renderPassFailInfo () {
+		const {type, requirementsMet} = this.state;
+
+		if(type === STUDENT) {
+			return <PassFailMessage requirementsMet={requirementsMet}/>;
+		}
+	}
+
 	render () {
 		if(!this.state.courseProgress || this.state.showDefaultHeader) {
 			return <div className="default-outline-header">{t('outline')}</div>;
@@ -220,9 +232,12 @@ class OutlineHeader extends React.Component {
 
 		return (
 			<HOC.ItemChanges item={this.props.course.PreferredAccess} onItemChange={this.onPreferredAccessChange}>
-				<div className="outline-progress-header">
-					<CircularProgress width={38} height={38} value={this.state.pctComplete} showPctSymbol={false} deficitFillColor="#b8b8b8"/>
-					{this.renderLabel()}
+				<div className="outline-header">
+					<div className="outline-progress-header">
+						<CircularProgress width={38} height={38} value={this.state.pctComplete} showPctSymbol={false} deficitFillColor="#b8b8b8"/>
+						{this.renderLabel()}
+					</div>
+					{this.renderPassFailInfo()}
 				</div>
 			</HOC.ItemChanges>
 		);
---
+++ src/progress/widgets/PassFailMessage.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
+import cx from 'classnames';
+
+import {RequirementDetails} from '../../pass-fail/requirement-details';
+
+const t = scoped('course.progress.widgets.PassFailMessage', {
+	successCongratulations: 'Congratulations!',
+	successMessage: 'You successfully completed this course.',
+	failMessage: 'You haven\'t met the requirements to complete this course yet.',
+	failDetails: 'View Details'
+});
+
+export default class PassFailMessage extends React.Component {
+	static propTypes = {
+		requirementsMet: PropTypes.bool
+	}
+
+	state = {}
+
+	renderPassInfo () {
+		return (
+			<div className="success-message">
+				<div className="congratulations">{t('successCongratulations')}</div>
+				<div className="message">{t('successMessage')}</div>
+			</div>
+		);
+	}
+
+	renderFailInfo () {
+		return (
+			<div className="fail-message">
+				<div className="message">{t('failMessage')}</div>
+				<div className="details-link" onClick={this.viewDetails}>{t('failDetails')}</div>
+			</div>
+		);
+	}
+
+	viewDetails = () => {
+		this.setState({
+			viewDetails: true
+		});
+	}
+
+	render () {
+		const {requirementsMet} = this.props;
+		const {viewDetails} = this.state;
+
+		return (
+			<div className={cx('pass-fail-message', { success: requirementsMet })}>
+				{requirementsMet ? this.renderPassInfo() : this.renderFailInfo()}
+				{viewDetails && <RequirementDetails onBeforeDismiss={() => this.setState({viewDetails: false})}/>}
+			</div>
+		);
+	}
+}
