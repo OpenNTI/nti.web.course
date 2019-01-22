e0c283018886facc50ea84bbd435dda21b2881c8
Ray Hatfield <ray.hatfield@nextthought.com>
2019-01-22 13:25:34 -0600

    use <circle> to draw circles

--- src/roster/columns/PercentComplete.jsx
+++ src/roster/columns/PercentComplete.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import Logger from '@nti/util-logger';
 
 import styles from './PercentComplete.css';
 
+const logger = Logger.get('roster.component.percent-complete');
 const cx = classnames.bind(styles);
 
-export default function PercentComplete ({className, percentage = 0}) {
+function clamp (p) {
+	if (p < 0 || p > 1) {
+		logger.warn(`Expected a fractional value between 0 and 1. Received ${p}. Clamping to stay in range.`);
+		return Math.min(Math.max(0, p), 1);
+	}
+	return p;
+}
 
+export default function PercentComplete ({className, percentage: p = 0}) {
+
+	const percentage = clamp(p);
 	const progressAsInt = global.parseInt(percentage * 100);
 	const positive = percentage > 0;
+	const radius = 15.9155; // circumference = 100; aligns dash-array values with percentages
+	const strokeWidth = 2;
+	const size = Math.ceil((radius + strokeWidth) * 2);
+	const strokeDasharray = `${progressAsInt} ${100 - progressAsInt}`;
+	const strokeDashoffset = 25; // shift so zero is at 12 o'clock
+
+	const props = {
+		cx: '50%',
+		cy: '50%',
+		r: radius,
+		strokeWidth,
+		strokeDashoffset,
+		fill: 'none',
+		stroke: '#eee'
+	};
+
+	const completedProps = {
+		...props,
+		strokeDasharray,
+		stroke: '#3fb34f'
+	};
 
 	return (
 		<div className={cx('percent-complete-container', {positive}, className)} data-progress-percent={percentage}>
-			<svg width="25" height="25" viewBox="0 0 36 36">
-				<path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" stroke="#eee" strokeWidth="2" strokeDasharray="100, 100" />
-				<path d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" fill="none" stroke="#3FB34F" strokeWidth="2" strokeDasharray={`${progressAsInt}, 100`}/>
+			<svg width="25" height="25" viewBox={`0 0 ${size} ${size}`}>
+				<circle {...props} />
+				{positive && (
+					<circle {...completedProps} />
+				)}
 			</svg>
 			<span className={cx('text')}>{progressAsInt}%</span>
 		</div>
