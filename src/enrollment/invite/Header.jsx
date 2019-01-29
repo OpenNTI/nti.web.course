f4e585cc2ac1586da0cc4a19c77db97f2ada8cb7
Ray Hatfield <ray.hatfield@nextthought.com>
2019-01-29 11:18:22 -0600

    use real course title, add help link

--- src/enrollment/invite/Header.jsx
+++ src/enrollment/invite/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {scoped} from '@nti/lib-locale';
+import { rawContent } from '@nti/lib-commons';
 
 const t = scoped('course.enrollment.invite.header', {
-	content: 'Invite others to take %(courseTitle)s. Add a message below and we’ll send your message along with a course link to the emails provided. Each email will receive a personalized invitation code.'
+	content: 'Invite others to take <span class="course-title">%(courseTitle)s</span>. Add a message below and we’ll send your message along with a course link to the emails provided. Each email will receive a personalized invitation code. <a href="https://help.nextthought.com/guide/part3.html#course-roster" target="_blank">More Information</a>'
 });
 
-export default function Header (props) {
+export default function Header ({course: {title: courseTitle = 'this course'} = {}}) {
 	return (
 		<header>
-			<div>{t('content', {courseTitle: 'The Reactified Roster Course'})}</div>
+			<div {...rawContent(t('content', {courseTitle}))} />
 		</header>
 	);
 }
+
+Header.propTypes = {
+	course: PropTypes.shape({
+		title: PropTypes.string
+	})
+};
+
