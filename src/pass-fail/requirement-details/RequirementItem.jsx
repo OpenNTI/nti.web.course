5b2ee1542b94bd43c07442939169ab234dc5b4da
Andrew Ligon <andrew.ligon@nextthought.com>
2019-02-22 11:04:33 -0600

    NTI-7495 fix floating point math issues

--- src/pass-fail/requirement-details/RequirementItem.jsx
+++ src/pass-fail/requirement-details/RequirementItem.jsx
@@ -9,12 +9,12 @@ export default class RequirementItem extends React.Component {
 	render () {
 		const { item } = this.props;
 		const grade = item.UserPointsReceived;
-		const score = (Math.floor((grade / item.TotalPoints) * 100) / 100);
+		const score = Math.floor((grade / item.TotalPoints) * 100);
 
 		return (
 			<div className="requirement-item req-row">
 				<div className="req-col req-item-title">{item.AssignmentTitle}</div>
-				<div className="req-col req-item-score">{score * 100}%</div>
+				<div className="req-col req-item-score">{score}%</div>
 				<div className="req-col req-item-target-score">{item.CompletionRequiredPassingPercentage * 100}%</div>
 			</div>
 		);
