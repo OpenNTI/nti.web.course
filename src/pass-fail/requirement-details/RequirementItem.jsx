8adfdd35ffb317e4d685a17d42e9c114e2be45d7
Andrew Ligon <andrew.ligon@nextthought.com>
2019-02-25 16:40:00 -0600

    NTI-7495 if the UserPointsReceived is NaN just display it instead of trying to convert it to a %

--- src/pass-fail/requirement-details/RequirementItem.jsx
+++ src/pass-fail/requirement-details/RequirementItem.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
+
+const t = scoped('course.pass-fail.requirements-details.RequirementItem', {
+	percentage: '%(score)s%%'
+});
+
+function getScore (item) {
+	const numeric = item.getNumericUserPointsReceived();
+
+	if (isNaN(numeric)) { return item.UserPointsReceived; }
+
+	const score = Math.round((numeric / item.TotalPoints) * 100);
+
+	return t('percentage', {score});
+}
 
 export default class RequirementItem extends React.Component {
 	static propTypes = {
@@ -8,13 +23,12 @@ export default class RequirementItem extends React.Component {
 
 	render () {
 		const { item } = this.props;
-		const grade = item.UserPointsReceived;
-		const score = Math.floor((grade / item.TotalPoints) * 100);
+		const score = getScore(item);
 
 		return (
 			<div className="requirement-item req-row">
 				<div className="req-col req-item-title">{item.AssignmentTitle}</div>
-				<div className="req-col req-item-score">{score}%</div>
+				<div className="req-col req-item-score">{score}</div>
 				<div className="req-col req-item-target-score">{item.CompletionRequiredPassingPercentage * 100}%</div>
 			</div>
 		);
