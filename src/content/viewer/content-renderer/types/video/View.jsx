a7490afb068c38db8a23140b61e95b9924a58aea
Andrew Ligon <andrew.ligon@nextthought.com>
2019-04-08 10:55:02 -0500

    the sidebar got moved into transcripted video so we don't need to import Notes

--- src/content/viewer/content-renderer/types/video/View.jsx
+++ src/content/viewer/content-renderer/types/video/View.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {TranscriptedVideo} from '@nti/web-content';
-import {Notes} from '@nti/web-discussions';
 
 import TypeRegistry from '../Registry';
 
