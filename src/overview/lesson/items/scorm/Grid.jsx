2afd2c43fd0dae64c055ac6de35b2889b8b79f03
Andrew Ligon <andrew.ligon@nextthought.com>
2019-06-11 18:19:08 -0500

    style the grid item

--- src/overview/lesson/items/scorm/Grid.jsx
+++ src/overview/lesson/items/scorm/Grid.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Card} from '@nti/web-commons';
+import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
+import {AssetIcon, Button, List} from '@nti/web-commons';
 import {LinkTo} from '@nti/web-routing';
 
 import PaddedContainer from '../../common/PaddedContainer';
 
+import Styles from './Grid.css';
+
+const cx = classnames.bind(Styles);
+const t = scoped('course.overview.lesson.items.scorm.Grid', {
+	open: 'Open'
+});
 
 LessonOverviewScormGridItem.propTypes = {
 	item: PropTypes.object.isRequired,
 	course: PropTypes.object,
-	commentLabel: PropTypes.node,
 	requiredLabel: PropTypes.node,
 	noProgress: PropTypes.bool
 };
-export default function LessonOverviewScormGridItem ({item, course, commentLabel, requiredLabel, noProgress}) {
+export default function LessonOverviewScormGridItem ({item, course, requiredLabel}) {
 	return (
 		<PaddedContainer>
 			<LinkTo.Object object={item}>
-				<div className="card">
-					<Card
-						data-ntiid={item.NTIID}
-						item={item}
-						contentPackage={course}
-						labels={[requiredLabel, commentLabel]}
-						noProgress={noProgress}
-					/>
+				<div className={cx('scorm-grid-card')}>
+					{item.icon && (<AssetIcon className={cx('asset-icon')} src={item.icon} mimeType={item.MimeType} />)}
+					<div className={cx('meta')}>
+						<div className={cx('title-container')}>
+							<div className={cx('title')}>{item.title}</div>
+							<Button className={cx('open-button')} rounded>{t('open')}</Button>
+						</div>
+						<List.SeparatedInline className={cx('list')}>
+							{requiredLabel}
+						</List.SeparatedInline>
+						<div className={cx('description')}>{item.description}</div>
+					</div>
 				</div>
 			</LinkTo.Object>
 		</PaddedContainer>
 	);
+
+	// return (
+	// 	<PaddedContainer>
+	// 		<LinkTo.Object object={item}>
+	// 			<div className="card">
+	// 				<Card
+	// 					data-ntiid={item.NTIID}
+	// 					item={item}
+	// 					contentPackage={course}
+	// 					labels={[requiredLabel, commentLabel]}
+	// 					noProgress={noProgress}
+	// 				/>
+	// 			</div>
+	// 		</LinkTo.Object>
+	// 	</PaddedContainer>
+	// );
 }
