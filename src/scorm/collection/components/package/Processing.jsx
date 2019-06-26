be67cdd2c40ba19cc391305c6625a0c29e8e8699
Andrew Ligon <andrew.ligon@nextthought.com>
2019-06-26 14:06:02 -0500

    style the processing state

--- src/scorm/collection/components/package/Processing.jsx
+++ src/scorm/collection/components/package/Processing.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
+import {Loading} from '@nti/web-commons';
+
+import Title from '../../common/Title';
+import SubTitle from '../../common/SubTitle';
+
+import Styles from './Processing.css';
+
+const cx = classnames.bind(Styles);
+const t = scoped('course.scorm.collection.components.package.Processing', {
+	label: 'Processing',
+	message: 'May Take a Couple of Minutes'
+});
 
 
 export default class ProcessingPackage extends React.PureComponent {
@@ -46,9 +60,17 @@ export default class ProcessingPackage extends React.PureComponent {
 
 
 	render () {
+		const {package: pack} = this.props;
+
 		return (
-			<div>
-				Processing
+			<div className={cx('processing-package')}>
+				<Loading.Spinner className={cx('loading')} />
+				<div className={cx('title-container')}>
+					<Title>{pack.title || pack.fileName}</Title>
+					<div className={cx('sub-titles')}>
+						<SubTitle blue>{t('label')}</SubTitle> <SubTitle dark>{t('message')}</SubTitle>
+					</div>
+				</div>
 			</div>
 		);
 	}
