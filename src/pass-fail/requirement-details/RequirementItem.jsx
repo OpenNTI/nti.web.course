f079a3683475aaf5f3ff508e94f7e0d8271916bc
Andrew Ligon <andrew.ligon@nextthought.com>
2019-06-27 15:59:06 -0500

    NTI-8104 handle failed scorm requirements

--- src/pass-fail/requirement-details/RequirementItem.jsx
+++ src/pass-fail/requirement-details/RequirementItem.jsx
@@ -3,18 +3,31 @@ import PropTypes from 'prop-types';
 import {scoped} from '@nti/lib-locale';
 
 const t = scoped('course.pass-fail.requirements-details.RequirementItem', {
-	percentage: '%(score)s%%'
+	percentage: '%(score)s%%',
+	notSatisfactory: 'Not Satisfactory',
+	satisfactory: 'Satisfactory'
 });
 
-function getScore (item) {
-	const numeric = item.getNumericUserPointsReceived();
+const TYPES = {
+	'application/vnd.nextthought.assignmentcompletionmetadata': {
+		title: item => item.AssignmentTitle,
+		score: (item) => {
+			const numeric = item.getNumericUserPointsReceived();
 
-	if (isNaN(numeric)) { return item.UserPointsReceived; }
+			if (isNaN(numeric)) { return item.UserPointsReceived; }
 
-	const score = Math.round((numeric / item.TotalPoints) * 100);
+			const score = Math.round((numeric / item.TotalPoints) * 100);
 
-	return t('percentage', {score});
-}
+			return t('percentage', {score});
+		},
+		targetScore: item => t('percentage', {score: item.CompletionRequiredPassingPercentage * 100})
+	},
+	'application/vnd.nextthought.scormcompletionmetadata': {
+		title: item => item.ScormContentInfoTitle,
+		score: item => item.Success ? t('notSatisfactory') : t('satisfactory'),
+		targetScore: () => '-'
+	}
+};
 
 export default class RequirementItem extends React.Component {
 	static propTypes = {
@@ -23,13 +36,15 @@ export default class RequirementItem extends React.Component {
 
 	render () {
 		const { item } = this.props;
-		const score = getScore(item);
+		const type = TYPES[item.MimeType];
+
+		if (!type) { return null; }
 
 		return (
 			<div className="requirement-item req-row">
-				<div className="req-col req-item-title">{item.AssignmentTitle}</div>
-				<div className="req-col req-item-score">{score}</div>
-				<div className="req-col req-item-target-score">{item.CompletionRequiredPassingPercentage * 100}%</div>
+				<div className="req-col req-item-title">{type.title(item)}</div>
+				<div className="req-col req-item-score">{type.score(item)}</div>
+				<div className="req-col req-item-target-score">{type.targetScore(item)}</div>
 			</div>
 		);
 	}
