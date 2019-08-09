83ff56bb540af8337a021e388cd7a8e3f7af054e
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-08 20:19:07 -0500

    hide the course start and end date description for now

--- src/info/inline/components/enddate/Disclaimer.jsx
+++ src/info/inline/components/enddate/Disclaimer.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import {scoped} from '@nti/lib-locale';
+// import React from 'react';
+// import {scoped} from '@nti/lib-locale';
 
-import Description from '../../widgets/Description';
+// import Description from '../../widgets/Description';
 
-const t = scoped('course.info.inline.components.enddate.Disclaimer', {
-	description: 'When class is officially over.'
-});
+// const t = scoped('course.info.inline.components.enddate.Disclaimer', {
+// 	description: 'When class is officially over.'
+// });
 
 export default function EndDateDisclaimer () {
-	return (
-		<Description>{t('description')}</Description>
-	);
+	return null;
+
+	// return (
+	// 	<Description>{t('description')}</Description>
+	// );
 }
--- src/info/inline/components/startdate/Disclaimer.jsx
+++ src/info/inline/components/startdate/Disclaimer.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import {scoped} from '@nti/lib-locale';
+// import React from 'react';
+// import {scoped} from '@nti/lib-locale';
 
-import Description from '../../widgets/Description';
+// import Description from '../../widgets/Description';
 
-const t = scoped('course.info.inline.components.startdate.Disclaimer', {
-	description: 'Delay when people can start.'
-});
+// const t = scoped('course.info.inline.components.startdate.Disclaimer', {
+// 	description: 'Delay when people can start.'
+// });
 
 export default function StartDateDisclaimer () {
-	return (
-		<Description>{t('description')}</Description>
-	);
+	return null;
+	// return (
+	// 	<Description>{t('description')}</Description>
+	// );
 }
