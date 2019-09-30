0e094d9fc868ec74fdc78a42b6d8b6fb4cdb21e8
Isaac Peterson <isaac.peterson@nextthought.com>
2019-09-30 13:32:04 -0500

    localized variables

--- src/card/parts/Image.jsx
+++ src/card/parts/Image.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Presentation} from '@nti/web-commons';
 
-
 CourseCardImage.propTypes = {
 	course: PropTypes.object.isRequired
 };
@@ -10,7 +9,7 @@ export default function CourseCardImage ({course}) {
 	return (
 		<div className="nti-course-card-image">
 			<Presentation.Asset contentPackage={course} propName="src" type="landing">
-				<img />
+				<img alt="" />
 			</Presentation.Asset>
 		</div>
 	);
