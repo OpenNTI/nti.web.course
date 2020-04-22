2a5350f581120cbb7d3272653b28e77f28c67887
Andrew Ligon <andrew.ligon@nextthought.com>
2020-04-22 15:19:51 -0500

    NTI-9177 NTI-9175 if the store enrollment is turned off don't say there is a price

--- src/info/inline/components/pricing/utils/get-price.js
+++ src/info/inline/components/pricing/utils/get-price.js
@@ -2,6 +2,7 @@ import getPurchasable from './get-purchasable';
 
 export default function getPrice (course) {
 	const purchasable = getPurchasable(course);
+	const enrollmentOption = course?.getEnrollmentOptions()?.getEnrollmentOptionForPurchase();
 
-	return purchasable && purchasable.Public !== false ? {amount: (purchasable.amount ?? 0) * 100, currency: purchasable.currency} : null;
+	return purchasable && enrollmentOption?.enabled ? {amount: (purchasable.amount ?? 0) * 100, currency: purchasable.currency} : null;
 }
