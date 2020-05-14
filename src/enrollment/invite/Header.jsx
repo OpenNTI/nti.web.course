88aa8ca3a7b288b5afa199dd0c2ff6668346bd95
Andrew Ligon <andrew.ligon@nextthought.com>
2020-05-14 16:39:00 -0500

    NTI-9195 make it clear that invites bypass purchase options

--- src/enrollment/invite/Header.jsx
+++ src/enrollment/invite/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
 import {scoped} from '@nti/lib-locale';
-import { rawContent } from '@nti/lib-commons';
+import {Text} from '@nti/web-commons';
 
+import Styles from './Header.css';
+
+const cx = classnames.bind(Styles);
 const t = scoped('course.enrollment.invite.header', {
-	content: 'Invite others to take <span class="course-title">%(courseTitle)s</span>. Add a message below and we’ll send your message along with a course link to the emails provided. Each email will receive a personalized invitation code. <a href="https://help.nextthought.com/guide/part3.html#course-roster" target="_blank">More Information</a>'
+	title: 'Course Invitation',
+	content: 'Invite learners to join <span class="course-title">%(courseTitle)s</span>. Invitations provide direct access—bypassing any purchase options.'
 });
 
 export default function Header ({course: {title: courseTitle = 'this course'} = {}}) {
 	return (
-		<header>
-			<div {...rawContent(t('content', {courseTitle}))} />
+		<header className={cx('header')}>
+			<Text.Base className={cx('title')} localized>{t('title')}</Text.Base>
+			<Text.Base className={cx('description')} localized>{t('content', {courseTitle})}</Text.Base>
 		</header>
 	);
 }
