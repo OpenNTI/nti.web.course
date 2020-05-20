bd9987dd0f252c3f60a1d0811c56eb9b9f714804
Andrew Ligon <andrew.ligon@nextthought.com>
2020-05-19 21:03:45 -0500

    add a title to the page

--- src/info/page/Frame.jsx
+++ src/info/page/Frame.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
 import {Page, Hooks, Loading} from '@nti/web-commons';
+import {View} from '@nti/web-routing';
 
 import NavBar from './components/NavBar';
+
+const t = scoped('course.info.page.Frame', {
+	title: 'Course Info'
+});
 
 const {useResolver} = Hooks;
 const {isPending, isErrored, isResolved} = useResolver;
@@ -33,18 +39,20 @@ export default function CourseInfoFrame ({course, children, ...otherProps}) {
 	const courseParts = isResolved(resolver) ? resolver : null;
 
 	return (
-		<Page>
-			<Page.Navigation>
-				<NavBar {...courseParts} />
-			</Page.Navigation>
-			<Page.Content card={false}>
-				<Loading.Placeholder loading={loading} fallback={(<Page.Content.Loading />)}>
-					{error && (<Page.Content.Error error={error} />)}
-					{courseParts && React.Children.map(children, (child) => {
-						return React.cloneElement(child, {...courseParts, ...otherProps});
-					})}
-				</Loading.Placeholder>
-			</Page.Content>
-		</Page>
+		<View.WithTitle title={t('title')}>
+			<Page>
+				<Page.Navigation>
+					<NavBar {...courseParts} />
+				</Page.Navigation>
+				<Page.Content card={false}>
+					<Loading.Placeholder loading={loading} fallback={(<Page.Content.Loading />)}>
+						{error && (<Page.Content.Error error={error} />)}
+						{courseParts && React.Children.map(children, (child) => {
+							return React.cloneElement(child, {...courseParts, ...otherProps});
+						})}
+					</Loading.Placeholder>
+				</Page.Content>
+			</Page>
+		</View.WithTitle>
 	);
 }
