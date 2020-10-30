f53fd7f53f89a496f3190c3c1debf00e022bcd76
Andrew Ligon <andrew.ligon@nextthought.com>
2020-10-30 13:20:15 -0500

    fill in more of the student progress header

--- src/overview/outline/header/StudentProgress.jsx
+++ src/overview/outline/header/StudentProgress.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import {scoped} from '@nti/lib-locale';
 import {Hooks as SessionHooks} from '@nti/web-session';
-import {Page, Text, Hooks} from '@nti/web-commons';
+import {Page, Text, Hooks, Prompt, Iframe} from '@nti/web-commons';
 import {CircularProgress} from '@nti/web-charts';
+
+import PassFailMessage from '../../../progress/widgets/PassFailMessage';
 
 import Styles from './Styles.css';
 
@@ -17,7 +19,8 @@ const t = scoped('course.overview.outline.header.StudentProgress', {
 	itemsRemaining: {
 		one: '%(count)s Item Remaining',
 		other: '%(count)s Items Remaining'
-	}
+	},
+	certificateTitle: 'Certificate of Completion for %(title)s'
 });
 
 function useStudentProgress (course, active) {
@@ -50,8 +53,10 @@ function useStudentProgress (course, active) {
 const getIsCompleted = progress => Boolean(progress?.getCompletedDate());
 const getPercentComplete = progress => Math.floor((progress?.PercentageProgress ?? 0) * 100);
 const getRemainingCount = progress => (progress?.MaxPossibleProgress ?? 0) - (progress?.AbsoluteProgress ?? 0);
+const getRequirementsMet = progress => progress?.CompletedItem?.Success;
 
 const getCertLink = course => course.PreferredAccess.getLink('Certificate');
+const getTitle = course => course.PreferredAccess.CatalogEntry.title;
 
 StudentProgress.handles = (course) => course.PreferredAccess?.isEnrollment && Object.keys(course).includes('CompletionPolicy');
 StudentProgress.propTypes = {
@@ -68,6 +73,7 @@ export default function StudentProgress ({className, course, active, noCertifica
 
 	const progress = useStudentProgress(course, active);
 	const isCompleted = getIsCompleted(progress);
+	const requirementsMet = isCompleted && getRequirementsMet(progress);
 	const certLink = getCertLink(course);
 
 	const linkProps = {
@@ -75,20 +81,20 @@ export default function StudentProgress ({className, course, active, noCertifica
 		className: Styles['progress-header']
 	};
 
-	if (isCompleted) {
-		if (certLink && !noCertificateFrame) {
+	if (isCompleted && certLink) {
+		if (!noCertificateFrame) {
 			linkProps.href = certLink;
 			linkProps.target = '_blank';
-		} else if (certLink) {
+		} else {
 			linkProps.onClick = () => setShowCert(true);
 		}
-	} else {
+	} else if (!isCompleted) {
 		linkProps.onClick = () => setShowRemaining(true);
 	}
 
 	let subLabel = '';
 
-	if (isCompleted) {
+	if (isCompleted && requirementsMet) {
 		subLabel = certLink ? t('viewCertificate') : t('completed');
 	} else {
 		subLabel = t('itemsRemaining', {count: getRemainingCount(progress)});
@@ -96,18 +102,39 @@ export default function StudentProgress ({className, course, active, noCertifica
 
 	return (
 		<Outline.Header as="a" {...linkProps} className={cx(Styles['student-progress-header'], className)}>
-			<div className={Styles['progress-header']}>
-				<div className={Styles.percentage}>
-					<CircularProgress
-						width={38}
-						height={38}
-						value={getPercentComplete(progress)}
-						showPctSymbol={false}
-						deficitFillColor="#b8b8b8"
-					/>
+			<div>
+				<div className={Styles['progress-header']}>
+					<div className={Styles.percentage}>
+						<CircularProgress
+							width={38}
+							height={38}
+							value={getPercentComplete(progress)}
+							showPctSymbol={false}
+							deficitFillColor="#b8b8b8"
+						/>
+					</div>
+					<Text.Base className={Styles.label}>{t('label')}</Text.Base>
+					<Text.Base className={Styles['sub-label']}>{subLabel}</Text.Base>
 				</div>
-				<Text.Base className={Styles.label}>{t('label')}</Text.Base>
-				<Text.Base className={Styles['sub-label']}>{subLabel}</Text.Base>
+				{isCompleted && (
+					<PassFailMessage course={course} requirementsMet={getRequirementsMet(requirementsMet)} />
+				)}
+				{showCert && (
+					<Prompt.Dialog onBeforeDismiss={() => setShowCert(false)}>
+						<Iframe
+							downloadable
+							src={certLink}
+							title={t('certificateTitle', {title: getTitle(course)})}
+						/>
+					</Prompt.Dialog>
+				)}
+				{showRemaining && (
+					<Prompt.Dialog onBeforeDismiss={() => setShowRemaining(false)}>
+						<div>
+							Remaining Items
+						</div>
+					</Prompt.Dialog>
+				)}
 			</div>
 		</Outline.Header>
 	);
