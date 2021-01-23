38d43906642ee956dcb7d0a1d34d00c568e39db1
Andrew Ligon <andrew.ligon@nextthought.com>
2021-01-23 17:29:17 -0600

    lint

--- src/admin-tools/advanced/completion/Credit.jsx
+++ src/admin-tools/advanced/completion/Credit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 // import TranscriptCredit from '../../../info/inline/components/transcriptcredit';
 // import Section from '../../../info/inline/components/Section';
