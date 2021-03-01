5e0204643c7d08538c32153dc2a4c878b67bd29b
Ray Hatfield <ray.hatfield@gmail.com>
2021-03-01 14:12:29 -0600

    add type prop to CourseCardImage

--- src/card/parts/Image.jsx
+++ src/card/parts/Image.jsx
@@ -5,11 +5,12 @@ import { Presentation, Image } from '@nti/web-commons';
 
 CourseCardImage.propTypes = {
 	course: PropTypes.object.isRequired,
+	type: PropTypes.string,
 };
-export default function CourseCardImage({ course }) {
+export default function CourseCardImage({ course, type = 'landing' }) {
 	return (
 		<div className="nti-course-card-image">
-			<Presentation.Asset contentPackage={course} type="landing">
+			<Presentation.Asset contentPackage={course} type={type}>
 				<Image.Deferred alt="" />
 			</Presentation.Asset>
 		</div>
