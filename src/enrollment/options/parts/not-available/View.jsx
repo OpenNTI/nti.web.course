2317c3650877ff53a00044ec7489911faa9b7f25
Andrew Ligon <andrew.ligon@nextthought.com>
2021-03-03 12:20:16 -0600

    NTI-10360 let users know that they can enroll with an invite code

--- src/enrollment/options/parts/not-available/View.jsx
+++ src/enrollment/options/parts/not-available/View.jsx
@@ -4,82 +4,25 @@ import { LinkTo } from '@nti/web-routing';
 
 import Title from '../../common/Title';
 import Description from '../../common/Description';
+import Button from '../../common/Button';
 
 const t = scoped('course.enrollment.options.NotAvailable', {
-	title: 'Not Available',
-	description:
-		'This course is not available at this time. For further assistance please contact the {{ContactLink`help desk.`}}',
+	title: 'Invitation only',
+	description: 'This course is available via invitation only.',
+	redeem: 'Redeem Invitation Code'
 });
 
-function ContactLink(props) {
-	const obj = {
-		type: 'contact-support',
-		message: '',
-		subject: '',
-	};
-
-	return <LinkTo.Object object={obj} {...props} />;
-}
-
-const LocaleComponents = {
-	ContactLink,
-};
-
-const MARK_UP_REGEX = /{{([^}}]*)}}/g;
-const getMatches = str => {
-	const matches = [];
-
-	let match;
-	while ((match = MARK_UP_REGEX.exec(str))) {
-		matches.push(match);
-	}
-
-	return matches;
-};
-
-const MATCH_REGEX = /^([^`]*)`([^`]*)`$/;
-const getComponentForMatch = cmpMatch => {
-	const match = cmpMatch.match(MATCH_REGEX);
-
-	if (!match) {
-		return cmpMatch;
-	}
-
-	const html = match[2];
-	const CmpName = match[1];
-	const Cmp = LocaleComponents[CmpName];
-
-	if (!Cmp) {
-		return html;
-	}
-
-	return <Cmp key={cmpMatch}>{html}</Cmp>;
-};
-
-function compileLocaleString(str) {
-	const matches = getMatches(str);
-
-	if (!matches || !matches.length) {
-		return str;
-	}
-
-	let pointer = 0;
-	let compiled = [];
-
-	for (let match of matches) {
-		compiled.push(str.substring(pointer, match.index));
-		compiled.push(getComponentForMatch(match[1]));
-		pointer = match.index + match[0].length;
-	}
-
-	return compiled;
-}
 
 export default function CourseEnrollmentOptionsNotAvailable() {
 	return (
 		<div>
 			<Title>{t('title')}</Title>
-			<Description>{compileLocaleString(t('description'))}</Description>
+			<Description>{t('description')}</Description>
+			<LinkTo.Object object={{type: 'redeem-course-code'}}>
+				<Button>
+					{t('redeem')}
+				</Button>
+			</LinkTo.Object>
 		</div>
 	);
 }
