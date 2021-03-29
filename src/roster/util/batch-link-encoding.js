355ab86275b2b8fab7ac229b411e2bf7b309346b
Jonathan Grimes <jsg2021@gmail.com>
2021-03-29 14:28:57 -0500

    [NTI-10429] fix tests

--- src/roster/util/batch-link-encoding.js
+++ src/roster/util/batch-link-encoding.js
@@ -1,5 +1,3 @@
-import URL from 'url';
-
 import { Base64 } from 'js-base64';
 import QS from 'query-string';
 
@@ -8,16 +6,16 @@ import Logger from '@nti/util-logger';
 const logger = Logger.get('roster.utils.batch-link-encoding');
 
 export function encodeBatchLink(link) {
-	return Base64.encode(link);
+	return encodeURIComponent(Base64.encode(link));
 }
 
 export function decodeBatchLink(encoded) {
-	return Base64.decode(encoded);
+	return Base64.decode(decodeURIComponent(encoded));
 }
 
 export function parametersFromLink(link) {
 	try {
-		const { search } = URL.parse(link);
+		const { search } = new URL(link);
 		return QS.parse(search);
 	} catch (e) {
 		logger.warn('unable to parse link. %o', link);
