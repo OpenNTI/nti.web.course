2def5de7309ae2257fcd9cf2be7f3f54893ab26d
Andrew Ligon <aligon90@gmail.com>
2021-03-29 18:31:23 -0500

    NTI-10483 change the text to primary-grey if the white isn't readable

--- src/overview/lesson/items/group/View.jsx
+++ src/overview/lesson/items/group/View.jsx
@@ -2,12 +2,15 @@ import './View.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import {Color} from '@nti/lib-commons';
 
 import { Grid, List } from '../../Constants';
 import { collateDiscussions } from '../discussion/Collator';
 import Registry from '../Registry';
 import Items from '../View';
 
+const White = Color.fromHex('#fff');
+const settings = {level:'AA',size:'large'};
 export default class LessonOverviewGroup extends React.Component {
 	static propTypes = {
 		item: PropTypes.object,
@@ -18,12 +21,14 @@ export default class LessonOverviewGroup extends React.Component {
 		const { item, layout, ...otherProps } = this.props;
 		const { title, accentColor, Items: items } = item;
 		const isGrid = layout === Grid;
+		const dark = !White.a11y.isReadable(accentColor, settings);
 
 		return !items || !items.length ? null : (
 			<div
 				className={cx('lesson-overview-group', {
 					[List]: !isGrid,
 					[Grid]: isGrid,
+					dark
 				})}
 			>
 				{isGrid && (
