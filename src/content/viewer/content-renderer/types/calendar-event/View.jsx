6042e060366fbbee252b937dee5b4462db7c4bd0
Jonathan Grimes <jsg2021@gmail.com>
2021-05-19 14:16:29 -0500

    class => function

--- src/content/viewer/content-renderer/types/calendar-event/View.jsx
+++ src/content/viewer/content-renderer/types/calendar-event/View.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Models } from '@nti/lib-interfaces';
 import { Event } from '@nti/web-calendar';
 import { Layouts } from '@nti/web-commons';
 
@@ -14,40 +15,35 @@ const View = styled(Event.View)`
 	}
 `;
 
-const MIME_TYPES = {
-	'application/vnd.nextthought.nticalendareventref': true,
+CourseContentViewerRendererCalendarEvent.propTypes = {
+	location: PropTypes.shape({
+		item: PropTypes.object,
+	}),
+	course: PropTypes.object.isRequired,
 };
 
-const handles = obj => {
-	const { location } = obj || {};
-	const { item } = location || {};
+export default function CourseContentViewerRendererCalendarEvent({
+	course,
+	location,
+}) {
+	const { item: { CalendarEvent: event } = {} } = location || {};
 
-	return item && MIME_TYPES[item.MimeType];
-};
-
-export default class CourseContentViewerRendererCalendarEvent extends React.Component {
-	static propTypes = {
-		location: PropTypes.shape({
-			item: PropTypes.object,
-		}),
-		course: PropTypes.object.isRequired,
-	};
-
-	render() {
-		const { course, location } = this.props;
-		const { item: { CalendarEvent: event } = {} } = location || {};
-
-		if (!event) {
-			return null;
-		}
-
-		return (
-			<>
-				<Layouts.Aside component={Sidebar} course={course} />
-				<View event={event} dialog={false} controls={false} />
-			</>
-		);
+	if (!event) {
+		return null;
 	}
+
+	return (
+		<>
+			<Layouts.Aside component={Sidebar} course={course} />
+			<View event={event} dialog={false} controls={false} />
+		</>
+	);
 }
 
-TypeRegistry.register(handles)(CourseContentViewerRendererCalendarEvent);
+TypeRegistry.register(obj => {
+	const { MimeType } = obj?.location?.item || {};
+	return (
+		MimeType &&
+		Models.calendar.CalendarEventRef.MimeTypes.includes(MimeType)
+	);
+})(CourseContentViewerRendererCalendarEvent);
