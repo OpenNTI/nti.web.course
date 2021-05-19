4110b597b4562d42488f1b989fd3e1af62d2c270
Jonathan Grimes <jsg2021@gmail.com>
2021-05-19 11:45:18 -0500

    cleanup

--- src/content/viewer/View.jsx
+++ src/content/viewer/View.jsx
@@ -1,90 +1,74 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames/bind';
+import cx from 'classnames';
 
-import { decorate } from '@nti/lib-commons';
 import { getScrollParent, scrollElementTo } from '@nti/lib-dom';
-import { Layouts, Decorators, Page } from '@nti/web-commons';
+import { Layouts, Page, useExternClassName } from '@nti/web-commons';
 import { getHistory } from '@nti/web-routing';
 
-import Styles from './View.css';
 import Content from './content-renderer';
 import Header from './parts/Header';
 import UpNext from './parts/UpNext';
+import styles from './View.css';
 
 const { Aside } = Layouts;
-const cx = classnames.bind(Styles);
 
-class CourseContentViewer extends React.Component {
-	static ContentTypes = Content.ContentTypes;
+CourseContentViewer.ContentTypes = Content.ContentTypes;
+CourseContentViewer.propTypes = {
+	aside: PropTypes.bool,
+	error: PropTypes.any,
+	location: PropTypes.shape({
+		item: PropTypes.object,
+		totalPages: PropTypes.number,
+		currentPage: PropTypes.number,
+	}),
+};
 
-	static propTypes = {
-		className: PropTypes.string,
-		error: PropTypes.any,
-		location: PropTypes.shape({
-			item: PropTypes.object,
-			totalPages: PropTypes.number,
-			currentPage: PropTypes.number,
-		}),
+export default function CourseContentViewer({
+	className,
+	error,
+	location,
+	aside = true,
+	...otherProps
+}) {
+	useExternClassName('nti-course-content-open');
+	const domNode = useRef();
+	const { current: prev } = useRef({});
 
-		noAside: PropTypes.bool,
-	};
-
-	constructor(props) {
-		super(props);
-		this.domNode = React.createRef();
-	}
-
-	componentDidUpdate({ location: prevLoc }) {
-		const { location } = this.props;
-
-		const i = x => (x || {}).item;
-
-		if (i(location) !== i(prevLoc)) {
-			const scroller = getScrollParent(this.domNode.current);
+	useEffect(() => {
+		const i = x => x?.item;
+		if (i(location) !== i(prev.location)) {
+			const scroller = getScrollParent(domNode.current);
 
 			if (scroller) {
 				scrollElementTo(scroller, 0, 0);
 			}
 		}
-	}
 
-	render() {
-		const {
-			className,
-			error,
-			location,
-			noAside,
-			...otherProps
-		} = this.props;
-		const loading = !location;
-		const Cmp = noAside ? 'div' : Aside.Container;
+		prev.location = location;
+	}, [location]);
 
-		return (
-			<Cmp
-				ref={this.domNode}
-				className={cx('nti-course-content', className, { loading })}
-			>
-				<div className={cx('contents')}>
-					<Header location={location} {...otherProps} />
-					{error && (
-						<Page.Content.NotFound
-							error={error}
-							actions={[
-								Page.Content.NotFound.getBackAction(
-									getHistory()
-								),
-							]}
-						/>
-					)}
-					{!error && <Content location={location} {...otherProps} />}
-					<UpNext location={location} {...otherProps} />
-				</div>
-			</Cmp>
-		);
-	}
+	const loading = !location;
+	const Cmp = aside ? Aside.Container : 'div';
+
+	return (
+		<Cmp
+			ref={domNode}
+			className={cx('nti-course-content', className, { loading })}
+		>
+			<div className={styles.contents}>
+				<Header location={location} {...otherProps} />
+				{error && (
+					<Page.Content.NotFound
+						error={error}
+						actions={[
+							Page.Content.NotFound.getBackAction(getHistory()),
+						]}
+					/>
+				)}
+				{!error && <Content location={location} {...otherProps} />}
+				<UpNext location={location} {...otherProps} />
+			</div>
+		</Cmp>
+	);
 }
-
-export default decorate(CourseContentViewer, [
-	Decorators.addClassToRoot('nti-course-content-open'),
-]);
