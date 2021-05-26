0160ba3069fd75b9fb516399fb75d6cbc8452dad
Jonathan Grimes <jsg2021@gmail.com>
2021-05-26 10:46:32 -0500

    migrate scss to inline css

--- src/card/parts/Image.jsx
+++ src/card/parts/Image.jsx
@@ -1,8 +1,21 @@
-import './Image.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Presentation, Image } from '@nti/web-commons';
+
+const ImageBox = styled.div`
+	overflow: hidden;
+
+	img {
+		display: block;
+		min-width: 100%;
+		width: auto;
+		height: 100%;
+		transform: scale(1);
+		opacity: 1;
+		transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+	}
+`;
 
 CourseCardImage.propTypes = {
 	course: PropTypes.object.isRequired,
@@ -10,10 +23,10 @@ CourseCardImage.propTypes = {
 };
 export default function CourseCardImage({ course, type = 'landing' }) {
 	return (
-		<div className="nti-course-card-image">
+		<ImageBox className="nti-course-card-image">
 			<Presentation.Asset contentPackage={course} type={type}>
 				<Image.Deferred alt="" />
 			</Presentation.Asset>
-		</div>
+		</ImageBox>
 	);
 }
