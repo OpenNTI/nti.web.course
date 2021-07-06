fb769dbdb61ae39f475ac0c0aba2ad33d898fdfb
Ray Hatfield <ray.hatfield@gmail.com>
2021-07-06 13:05:01 -0500

    NTI-10899 - change redeem button text for unauthenticated users

--- src/enrollment/options/parts/not-available/View.jsx
+++ src/enrollment/options/parts/not-available/View.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { scoped } from '@nti/lib-locale';
 import { LinkTo } from '@nti/web-routing';
+import { useService } from '@nti/web-commons';
 
 import Title from '../../common/Title';
 import Description from '../../common/Description';
@@ -10,19 +11,22 @@ import Button from '../../common/Button';
 const t = scoped('course.enrollment.options.NotAvailable', {
 	title: 'Invitation only',
 	description: 'This course is available via invitation only.',
-	redeem: 'Redeem Invitation Code'
+	redeem: {
+		authenticated: 'Redeem Invitation Code',
+		unauthenticated: 'Sign In To Get Started',
+	},
 });
 
-
 export default function CourseEnrollmentOptionsNotAvailable() {
+	const { isAnonymous } = useService();
+	const scope = isAnonymous ? 'unauthenticated' : 'authenticated';
+
 	return (
 		<div>
 			<Title>{t('title')}</Title>
 			<Description>{t('description')}</Description>
-			<LinkTo.Object object={{type: 'redeem-course-code'}}>
-				<Button>
-					{t('redeem')}
-				</Button>
+			<LinkTo.Object object={{ type: 'redeem-course-code' }}>
+				<Button>{t(['redeem', scope])}</Button>
 			</LinkTo.Object>
 		</div>
 	);
