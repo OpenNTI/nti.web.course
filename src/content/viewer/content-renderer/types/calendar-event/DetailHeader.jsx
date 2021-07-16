41a6d65956fab6dbea953ea4fa528eb2fbbadf05
Jonathan Grimes <jsg2021@gmail.com>
2021-07-16 12:37:22 -0500

    [NTI-10829] props don't perfectly line up... fix that

--- src/content/viewer/content-renderer/types/calendar-event/DetailHeader.jsx
+++ src/content/viewer/content-renderer/types/calendar-event/DetailHeader.jsx
@@ -1,15 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Models } from '@nti/lib-interfaces';
 import { Event } from '@nti/web-calendar';
 import { isFlag } from '@nti/web-client';
 
 import { Registry } from '../../../parts/Header';
 
+//#region Registration & Meta
+
 Registry.register(
 	item =>
 		Models.calendar.CalendarEventRef.MimeType === item.MimeType &&
 		item.CalendarEvent?.hasLink('list-attendance') &&
 		isFlag('event-check-ins'),
-	Event.DetailHeader
+	Header
 );
 
-export default Event.DetailHeader;
+/**
+ * @typedef HeadingProps
+ * @property {Models.calendar.CalendarEventRef} item - Event Reference
+ */
+Header.propTypes = {
+	item: PropTypes.instanceOf(Models.calendar.CalendarEventRef).isRequired,
+};
+//#endregion
+
+/**
+ * @param {HeadingProps} props
+ * @returns {React.ReactElement}
+ */
+export default function Header({ item, className }) {
+	const { CalendarEvent: event } = item;
+	return <Event.DetailHeader {...{ event, className }} />;
+}
