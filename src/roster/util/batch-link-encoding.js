012b7a3da46a984490afdadbf023e0a1a22b28a0
Jonathan Grimes <jsg2021@gmail.com>
2021-07-21 11:55:05 -0500

    drop query-string

--- src/roster/util/batch-link-encoding.js
+++ src/roster/util/batch-link-encoding.js
@@ -1,9 +1,12 @@
 import { Base64 } from 'js-base64';
-import QS from 'query-string';
 
 import Logger from '@nti/util-logger';
 
 const logger = Logger.get('roster.utils.batch-link-encoding');
+
+const ensureArray = x => (Array.isArray(x) ? x : [x]);
+const append = (obj, key, newValue) =>
+	(obj[key] = [...ensureArray(obj[key]), newValue]);
 
 export function encodeBatchLink(link) {
 	return encodeURIComponent(Base64.encode(link));
@@ -15,8 +18,18 @@ export function decodeBatchLink(encoded) {
 
 export function parametersFromLink(link) {
 	try {
-		const { search } = new URL(link);
-		return QS.parse(search);
+		const { searchParams } = new URL(link);
+
+		const params = {};
+		for (const [key, value] of searchParams) {
+			if (key in params) {
+				// handle duplicate params...
+				append(params, key, value);
+			} else {
+				params[key] = value;
+			}
+		}
+		return params;
 	} catch (e) {
 		logger.warn('unable to parse link. %o', link);
 		return {};
