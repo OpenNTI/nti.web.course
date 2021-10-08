f8f4a4916cd06be69e17569e521fccbca43380ae
Jonathan Grimes <jsg2021@gmail.com>
2021-10-07 21:10:58 -0500

    Show precision as a number example

--- src/info/inline/components/transcriptcredit/managetypes/PrecisionInput.jsx
+++ src/info/inline/components/transcriptcredit/managetypes/PrecisionInput.jsx
@@ -1,5 +1,14 @@
 import { Input } from '@nti/web-commons';
 
+const getLabel = precision => {
+	const { locale } = new Intl.NumberFormat().resolvedOptions();
+	const formatter = new Intl.NumberFormat(locale, {
+		minimumFractionDigits: precision,
+		maximumFractionDigits: precision,
+	});
+	return formatter.format(0);
+};
+
 export function PrecisionInput(props) {
 	return (
 		<Input.Select
@@ -9,8 +18,8 @@ export function PrecisionInput(props) {
 				text-align: left;
 			`}
 		>
-			<Input.Select.Option value={1}>1</Input.Select.Option>
-			<Input.Select.Option value={2}>2</Input.Select.Option>
+			<Input.Select.Option value={1}>{getLabel(1)}</Input.Select.Option>
+			<Input.Select.Option value={2}>{getLabel(2)}</Input.Select.Option>
 		</Input.Select>
 	);
 }
