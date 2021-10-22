083f72da7a97dced6320806caf6b57f5353d8f1b
Jonathan Grimes <jsg2021@gmail.com>
2021-10-22 15:17:21 -0500

    fix proptype warning

--- src/info/inline/components/redemptioncodes/Edit.jsx
+++ src/info/inline/components/redemptioncodes/Edit.jsx
@@ -22,7 +22,7 @@ class RedemptionCodesEdit extends React.Component {
 			hasLink: PropTypes.func.isRequired,
 			postToLink: PropTypes.func.isRequired,
 		}),
-		items: PropTypes.object,
+		items: PropTypes.array,
 		onValueChange: PropTypes.func,
 		store: PropTypes.shape({
 			setCourse: PropTypes.func.isRequired,
